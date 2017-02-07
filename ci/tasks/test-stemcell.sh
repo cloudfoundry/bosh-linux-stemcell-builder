c3c51317bba4e0a6f5938273d83e6dd94c07783d
Shatarupa Nandi <snandi@pivotal.io>
2017-02-07 10:44:34 -0800

    Giving bosh-cli permission to execute.
    [#138649937](https://www.pivotaltracker.com/story/show/138649937)

--- ci/tasks/test-stemcell.sh
+++ ci/tasks/test-stemcell.sh
@@ -2,10 +2,12 @@
 
 set -exu
 
+export BOSH_BINARY_PATH=$(realpath bosh-cli/bosh-cli-*)
+chmod +x $BOSH_BINARY_PATH
+
 pushd bosh-linux-stemcell-builder
   export PATH=/usr/local/go/bin:$PATH
   export GOPATH=$(pwd)
-  export BOSH_BINARY_PATH=$(realpath bosh-cli/bosh-cli-*)
 
   pushd src/github.com/cloudfoundry/stemcell-acceptance-tests
     ./bin/test-smoke
