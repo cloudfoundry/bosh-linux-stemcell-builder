9c8cb6465cfebc5097aeba63d0b535363b651f85
Shatarupa Nandi <snandi@pivotal.io>
2017-02-08 10:48:37 -0800

    Update ci/configure.sh to point to the correct pipeline.yml
    [#138649937](https://www.pivotaltracker.com/story/show/138649937)
    
    Signed-off-by: Adrian Zankich <azankich@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -3,6 +3,6 @@
 set -eu
 
 fly -t production set-pipeline \
-  -p bosh:stemcells -c ci/pipelines/stemcells/pipeline.yml \
+  -p bosh:stemcells -c ci/pipeline.yml \
   -l <(lpass show --note "concourse:production pipeline:bosh:stemcells") \
   -l <(lpass show --note "bats-concourse-pool:vsphere secrets")
