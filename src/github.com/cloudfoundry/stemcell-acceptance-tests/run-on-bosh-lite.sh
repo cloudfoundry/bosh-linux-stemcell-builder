9e817caf5b82e6a1450265ca9055e5b2f17fe424
Shatarupa Nandi <snandi@pivotal.io>
2017-02-10 10:59:35 -0800

    Use ContainSubstring instead of grep for assertions.
    [#138649937](https://www.pivotaltracker.com/story/show/138649937)
    
    Signed-off-by: Jian Qiu Wang <wangjq@cn.ibm.com>

--- src/github.com/cloudfoundry/stemcell-acceptance-tests/run-on-bosh-lite.sh
+++ src/github.com/cloudfoundry/stemcell-acceptance-tests/run-on-bosh-lite.sh
@@ -5,8 +5,9 @@ export BOSH_CLIENT_SECRET=$(gobosh -e vbox int ~/workspace/bosh-deployment/vbox/
 export BOSH_ENVIRONMENT=https://192.168.50.6:25555
 export BOSH_CA_CERT=$(gobosh -e vbox int ~/workspace/bosh-deployment/vbox/creds.yml --path /director_ssl/ca)
 export BOSH_BINARY_PATH=gobosh
-export SYSLOG_RELEASE_PATH=syslog-release/release.tgz
-export STEMCELL_PATH=stemcell/stemcell.tgz
+export SYSLOG_RELEASE_PATH=../syslog-release/release.tgz
+export STEMCELL_PATH=../stemcell/stemcell.tgz
+export BOSH_stemcell_version=3312.15.dk2
 
 temp_file=$(mktemp)
 gobosh -e vbox int ~/workspace/bosh-deployment/vbox/creds.yml --path /jumpbox_ssh/private_key > $temp_file
@@ -16,4 +17,4 @@ export BOSH_GW_PRIVATE_KEY="$temp_file"
 export BOSH_GW_HOST=192.168.50.6
 export BOSH_GW_USER=jumpbox5
 
-ginkgo -r .
+ginkgo -r smoke
--- src/github.com/cloudfoundry/stemcell-acceptance-tests/smoke/smoke_test.go
+++ src/github.com/cloudfoundry/stemcell-acceptance-tests/smoke/smoke_test.go
@@ -14,53 +14,58 @@ import (
 
 var _ = Describe("Stemcell", func() {
 	Context("When syslog release has been deployed", func() {
-		cmdRunner := system.NewExecCmdRunner(boshlog.NewLogger(boshlog.LevelDebug))
+		cmdRunner := system.NewExecCmdRunner(boshlog.NewLogger(boshlog.LevelNone))
+		boshBinaryPath := os.Getenv("BOSH_BINARY_PATH")
 
 		Context("when auditd is monitoring access to modprobe", func() {
 			It("gets forwarded to the syslog storer", func() {
-				stdOut, stdErr, exitStatus, err := cmdRunner.RunCommand(os.Getenv("BOSH_BINARY_PATH"), "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_forwarder/0", "sudo modprobe -r floppy")
+				stdOut, _, exitStatus, err := cmdRunner.RunCommand(boshBinaryPath, "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_forwarder/0", "sudo modprobe -r floppy")
 				Expect(err).ToNot(HaveOccurred())
-				Expect(exitStatus).To(Equal(0), fmt.Sprintf("stdOut: %s \n stdErr: %s", stdOut, stdErr))
+				Expect(exitStatus).To(Equal(0))
 
-				stdOut, stdErr, exitStatus, err = cmdRunner.RunCommand(os.Getenv("BOSH_BINARY_PATH"), "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_storer/0", `grep 'COMMAND=/sbin/modprobe -r floppy' /var/vcap/store/syslog_storer/syslog.log`)
+				stdOut, _, exitStatus, err = cmdRunner.RunCommand(boshBinaryPath, "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_storer/0", `cat /var/vcap/store/syslog_storer/syslog.log`)
 				Expect(err).ToNot(HaveOccurred())
-				Expect(exitStatus).To(Equal(0), fmt.Sprintf("Syslog did not contain 'COMMAND=/sbin/modprobe -r floppy'! \n stdOut: %s \n stdErr: %s", stdOut, stdErr))
+				Expect(exitStatus).To(Equal(0))
+				Expect(stdOut).To(ContainSubstring("COMMAND=/sbin/modprobe -r floppy"))
 			})
 		})
 
 		Context("when logging to syslog", func() {
 			It("gets forwarded to the syslog storer", func() {
-				stdOut, stdErr, exitStatus, err := cmdRunner.RunCommand(os.Getenv("BOSH_BINARY_PATH"), "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_forwarder/0", "logger -t vcap some vcap message")
+				stdOut, stdErr, exitStatus, err := cmdRunner.RunCommand(boshBinaryPath, "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_forwarder/0", "logger -t vcap some vcap message")
 				Expect(err).ToNot(HaveOccurred())
 				Expect(exitStatus).To(Equal(0), fmt.Sprintf("stdOut: %s \n stdErr: %s", stdOut, stdErr))
 
-				stdOut, stdErr, exitStatus, err = cmdRunner.RunCommand(os.Getenv("BOSH_BINARY_PATH"), "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_storer/0", `grep 'some vcap message' /var/vcap/store/syslog_storer/syslog.log`)
+				stdOut, stdErr, exitStatus, err = cmdRunner.RunCommand(boshBinaryPath, "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_storer/0", `cat /var/vcap/store/syslog_storer/syslog.log`)
 				Expect(err).ToNot(HaveOccurred())
-				Expect(exitStatus).To(Equal(0), fmt.Sprintf("Syslog did not contain 'grep 'some vcap message'! \n stdOut: %s \n stdErr: %s", stdOut, stdErr))
+				Expect(exitStatus).To(Equal(0))
+				Expect(stdOut).To(ContainSubstring("some vcap message"))
 			})
 		})
 
 		Context("when syslog threshold limit is reached", func() {
 			It("should rotate the logs", func() {
-				stdOut, stdErr, exitStatus, err := cmdRunner.RunCommand(os.Getenv("BOSH_BINARY_PATH"), "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_forwarder/0", `logger "old syslog content" \
+				stdOut, stdErr, exitStatus, err := cmdRunner.RunCommand(boshBinaryPath, "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_forwarder/0", `logger "old syslog content" \
 	&& sudo bash -c "dd if=/dev/urandom count=10000 bs=1024 >> /var/log/syslog" \
 	&& sudo sed -i "s/0,15,30,45/\*/" /etc/cron.d/logrotate`)
 				Expect(err).ToNot(HaveOccurred())
-				Expect(exitStatus).To(Equal(0), fmt.Sprintf("stdOut: %s \n stdErr: %s", stdOut, stdErr))
+				Expect(exitStatus).To(Equal(0))
 
 				time.Sleep(62 * time.Second)
 
-				stdOut, stdErr, exitStatus, err = cmdRunner.RunCommand(os.Getenv("BOSH_BINARY_PATH"), "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_forwarder/0", `logger "new syslog content"`)
+				stdOut, stdErr, exitStatus, err = cmdRunner.RunCommand(boshBinaryPath, "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_forwarder/0", `logger "new syslog content"`)
 				Expect(err).ToNot(HaveOccurred())
-				Expect(exitStatus).To(Equal(0), fmt.Sprintf("Syslog did not contain 'grep 'some vcap message'! \n stdOut: %s \n stdErr: %s", stdOut, stdErr))
+				Expect(exitStatus).To(Equal(0), fmt.Sprintf("Could not log using logger on the syslog forwarder! \n stdOut: %s \n stdErr: %s", stdOut, stdErr))
 
-				stdOut, stdErr, exitStatus, err = cmdRunner.RunCommand(os.Getenv("BOSH_BINARY_PATH"), "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_forwarder/0", `sudo grep 'new syslog content' /var/vcap/data/root_log/syslog`)
+				stdOut, stdErr, exitStatus, err = cmdRunner.RunCommand(boshBinaryPath, "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_forwarder/0", `sudo cat /var/vcap/data/root_log/syslog`)
 				Expect(err).ToNot(HaveOccurred())
-				Expect(exitStatus).To(Equal(0), fmt.Sprintf("Syslog did not contain 'new syslog content'! \n stdOut: %s \n stdErr: %s", stdOut, stdErr))
+				Expect(exitStatus).To(Equal(0))
+				Expect(stdOut).To(ContainSubstring("new syslog content"))
 
-				stdOut, stdErr, exitStatus, err = cmdRunner.RunCommand(os.Getenv("BOSH_BINARY_PATH"), "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_forwarder/0", `sudo grep -vl 'old syslog content' /var/vcap/data/root_log/syslog`)
+				stdOut, stdErr, exitStatus, err = cmdRunner.RunCommand(boshBinaryPath, "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_storer/0", `sudo cat /var/vcap/data/root_log/syslog`)
 				Expect(err).ToNot(HaveOccurred())
-				Expect(exitStatus).To(Equal(0), fmt.Sprintf("Syslog contains content 'old syslog content' that should have been rotated! \n stdOut: %s \n stdErr: %s", stdOut, stdErr))
+				Expect(exitStatus).To(Equal(0), fmt.Sprintf("Could not read from syslog stdOut: %s \n stdErr: %s", stdOut, stdErr))
+				Expect(stdOut).NotTo(ContainSubstring("old syslog content"))
 			})
 		})
 
@@ -70,13 +75,13 @@ var _ = Describe("Stemcell", func() {
 			authLogAbsPath, err := filepath.Abs(tempFile.Name())
 			Expect(err).ToNot(HaveOccurred())
 
-			stdOut, stdErr, exitStatus, err := cmdRunner.RunCommand(os.Getenv("BOSH_BINARY_PATH"), "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_forwarder/0", `sudo cp /var/log/auth.log /tmp/ && sudo chmod 777 /tmp/auth.log`)
+			stdOut, stdErr, exitStatus, err := cmdRunner.RunCommand(boshBinaryPath, "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_forwarder/0", `sudo cp /var/log/auth.log /tmp/ && sudo chmod 777 /tmp/auth.log`)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(exitStatus).To(Equal(0), fmt.Sprintf("Could not create nested log path! \n stdOut: %s \n stdErr: %s", stdOut, stdErr))
 
-			stdOut, stdErr, exitStatus, err = cmdRunner.RunCommand(os.Getenv("BOSH_BINARY_PATH"), "-d", "bosh-stemcell-smoke-tests", "scp", "syslog_forwarder/0:/tmp/auth.log", authLogAbsPath)
+			stdOut, stdErr, exitStatus, err = cmdRunner.RunCommand(boshBinaryPath, "-d", "bosh-stemcell-smoke-tests", "scp", "syslog_forwarder/0:/tmp/auth.log", authLogAbsPath)
 			Expect(err).ToNot(HaveOccurred())
-			Expect(exitStatus).To(Equal(0), fmt.Sprintf("Could not download /var/log/auth.log \n stdOut: %s \n stdErr: %s", stdOut, stdErr))
+			Expect(exitStatus).To(Equal(0))
 
 			contents, err := ioutil.ReadAll(tempFile)
 			Expect(err).ToNot(HaveOccurred())
@@ -91,35 +96,38 @@ var _ = Describe("Stemcell", func() {
 			logFilePath, err := filepath.Abs(tempFile.Name())
 			Expect(err).ToNot(HaveOccurred())
 
-			stdOut, stdErr, exitStatus, err := cmdRunner.RunCommand(os.Getenv("BOSH_BINARY_PATH"), "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_forwarder/0", `sudo mkdir -p /var/vcap/sys/log/deep/path && sudo chmod 777 /var/vcap/sys/log/deep/path`)
+			stdOut, stdErr, exitStatus, err := cmdRunner.RunCommand(boshBinaryPath, "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_forwarder/0", `sudo mkdir -p /var/vcap/sys/log/deep/path && sudo chmod 777 /var/vcap/sys/log/deep/path`)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(exitStatus).To(Equal(0), fmt.Sprintf("Could not create nested log path! \n stdOut: %s \n stdErr: %s", stdOut, stdErr))
 
-			stdOut, stdErr, exitStatus, err = cmdRunner.RunCommand(os.Getenv("BOSH_BINARY_PATH"), "-d", "bosh-stemcell-smoke-tests", "scp", logFilePath, "syslog_forwarder/0:/var/vcap/sys/log/deep/path/deepfile.log")
+			_, _, exitStatus, err = cmdRunner.RunCommand(boshBinaryPath, "-d", "bosh-stemcell-smoke-tests", "scp", logFilePath, "syslog_forwarder/0:/var/vcap/sys/log/deep/path/deepfile.log")
 			Expect(err).ToNot(HaveOccurred())
-			Expect(exitStatus).To(Equal(0), fmt.Sprintf("Could not scp to /var/vcap/sys/log/deep/path/deepfile.log \n stdOut: %s \n stdErr: %s", stdOut, stdErr))
+			Expect(exitStatus).To(Equal(0))
 
 			time.Sleep(35 * time.Second)
 
-			stdOut, stdErr, exitStatus, err = cmdRunner.RunCommand(os.Getenv("BOSH_BINARY_PATH"), "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_storer/0", `sudo grep 'test-blackbox-message' /var/vcap/store/syslog_storer/syslog.log`)
+			stdOut, stdErr, exitStatus, err = cmdRunner.RunCommand(boshBinaryPath, "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_storer/0", `cat /var/vcap/store/syslog_storer/syslog.log`)
 			Expect(err).ToNot(HaveOccurred())
-			Expect(exitStatus).To(Equal(0), fmt.Sprintf("Syslog did not contain 'grep 'test-blackbox-message'! \n stdOut: %s \n stdErr: %s", stdOut, stdErr))
+			Expect(exitStatus).To(Equal(0))
+			Expect(stdOut).To(ContainSubstring("test-blackbox-message"))
 		})
 
 		It("#135979501: produces CEF logs for all incoming NATs and https requests", func() {
-			stdOut, stdErr, exitStatus, err := cmdRunner.RunCommand(os.Getenv("BOSH_BINARY_PATH"), "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_storer/0", `sudo grep 'CEF:0|CloudFoundry|BOSH|1|agent_api|get_task' /var/vcap/store/syslog_storer/syslog.log`)
+			stdOut, stdErr, exitStatus, err := cmdRunner.RunCommand(boshBinaryPath, "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_storer/0", `sudo cat /var/vcap/store/syslog_storer/syslog.log`)
 			Expect(err).ToNot(HaveOccurred())
-			Expect(exitStatus).To(Equal(0), fmt.Sprintf("Syslog did not contain 'CEF:0|CloudFoundry|BOSH|1|agent_api|get_task'! \n stdOut: %s \n stdErr: %s", stdOut, stdErr))
+			Expect(exitStatus).To(Equal(0), fmt.Sprintf("Could not read from syslog stdOut: %s \n stdErr: %s", stdOut, stdErr))
+			Expect(stdOut).To(ContainSubstring("CEF:0|CloudFoundry|BOSH|1|agent_api|get_task"))
 		})
 
 		It("#137987887: produces audit logs for use of specific binaries", func() {
-			stdOut, stdErr, exitStatus, err := cmdRunner.RunCommand(os.Getenv("BOSH_BINARY_PATH"), "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_forwarder/0", `chage -h`)
+			stdOut, stdErr, exitStatus, err := cmdRunner.RunCommand(boshBinaryPath, "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_forwarder/0", `chage -h`)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(exitStatus).To(Equal(0), fmt.Sprintf("Unable to run 'chage -h' \n stdOut: %s \n stdErr: %s", stdOut, stdErr))
 
-			stdOut, stdErr, exitStatus, err = cmdRunner.RunCommand(os.Getenv("BOSH_BINARY_PATH"), "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_storer/0", `sudo grep 'exe="/usr/bin/chage"' /var/vcap/store/syslog_storer/syslog.log`)
+			stdOut, stdErr, exitStatus, err = cmdRunner.RunCommand(boshBinaryPath, "-d", "bosh-stemcell-smoke-tests", "ssh", "syslog_storer/0", `cat /var/vcap/store/syslog_storer/syslog.log`)
 			Expect(err).ToNot(HaveOccurred())
-			Expect(exitStatus).To(Equal(0), fmt.Sprintf(`Syslog did not contain 'exe="/usr/bin/chage"! \n stdOut: %s \n stdErr: %s`, stdOut, stdErr))
+			Expect(exitStatus).To(Equal(0), fmt.Sprintf("Could not read from syslog stdOut: %s \n stdErr: %s", stdOut, stdErr))
+			Expect(stdOut).To(ContainSubstring(`exe="/usr/bin/chage"`))
 		})
 	})
 })
