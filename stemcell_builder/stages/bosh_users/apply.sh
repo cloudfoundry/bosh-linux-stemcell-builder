8a6b383ecd20abd377274be09a4614ca70e4bb47
Andre Duffeck <aduffeck@suse.com>
2017-04-07 10:18:58 +0200

    Explicitly create vcap group before creating the vcap user
    Not all distributions automatically create groups when adding users.
    
    Co-Authored-By: Mauro Morales <mamorales@suse.com>
    Co-Authored-By: Mario Manno <mmanno@suse.com>

--- stemcell_builder/stages/bosh_users/apply.sh
+++ stemcell_builder/stages/bosh_users/apply.sh
@@ -16,7 +16,8 @@ fi
 
 run_in_chroot $chroot "
 groupadd --system admin
-useradd -m --comment 'BOSH System User' vcap --uid 1000
+groupadd -f vcap
+useradd -m --comment 'BOSH System User' vcap --uid 1000 -g vcap
 chmod 700 ~vcap
 echo \"vcap:${bosh_users_password}\" | chpasswd
 echo \"root:${bosh_users_password}\" | chpasswd
