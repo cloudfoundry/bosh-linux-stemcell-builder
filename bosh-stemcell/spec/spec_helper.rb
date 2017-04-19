afbed41cf36f7f7928283c4d512c0362164bd9eb
Andre Duffeck <aduffeck@suse.com>
2017-04-19 09:19:38 +0200

    Make sure to load dependencies first
    Dir.glob does not guarantee a particular order in which the entries are
    returned. Depending on the machine the "shellout_type_assertions.rb"
    entry might be returned after files that depend on it which leads to an
    error.
    
    This is now fixed by loading it explicitly before loading all the other
    files in "support/".

--- bosh-stemcell/spec/spec_helper.rb
+++ bosh-stemcell/spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require 'rspec/its'
 require 'fakefs/spec_helpers'
+require 'support/shellout_type_assertions.rb'
 
 Dir.glob(File.expand_path('../support/**/*.rb', __FILE__)).each { |f| require(f) }
