af6ce1cf16af3062dddc16d1c135ba7da3044aff
Wang JianQiu <wangjq@cn.ibm.com>
2017-05-23 22:18:21 +0800

    new softlayer stemcell builder stuff

--- ci/tasks/build-softlayer.sh
+++ ci/tasks/build-softlayer.sh
@@ -69,7 +69,11 @@ sudo --preserve-env --set-home --user ubuntu -- /bin/bash --login -i <<SUDO
   cd bosh-linux-stemcell-builder
 
   bundle install --local
+<<<<<<< HEAD
   bundle exec rake stemcell:build[$IAAS,esxi,$OS_NAME,$OS_VERSION,bosh-os-images,bosh-$OS_NAME-$OS_VERSION-os-image.tgz]
+=======
+  bundle exec rake stemcell:build[$IAAS,esxi,$OS_NAME,$OS_VERSION,go,bosh-os-images,bosh-$OS_NAME-$OS_VERSION-os-image.tgz]
+>>>>>>> new softlayer stemcell builder stuff
   rm -f ./tmp/base_os_image.tgz
 SUDO
 
