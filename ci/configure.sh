ca4db5b4016ab4b344cbd83c7c17bd4fd59ad5a4
J Evelyn Quintana <jquintana@pivotal.io>
2017-07-11 16:44:52 -0700

    use correct CI secrets for pipeline
    [#147702091](https://www.pivotaltracker.com/story/show/147702091)

--- ci/configure.sh
+++ ci/configure.sh
@@ -4,5 +4,5 @@ set -eu
 
 fly -t production set-pipeline \
   -p bosh:stemcells -c ci/pipeline.yml \
-  -l <(lpass show --note "concourse:production pipeline:bosh:stemcells") \
+  -l <(lpass show --note "concourse:production pipeline:bosh:stemcells:3421.x") \
   -l <(lpass show --note "bats-concourse-pool:vsphere secrets")
