a563afc7e48222a67f0c29f4b05981d15b18666e
J Evelyn Quintana <jquintana@pivotal.io>
2017-07-12 14:14:06 -0700

    3421 configuration should not affect main branch
    [#147702091](https://www.pivotaltracker.com/story/show/147702091)

--- ci/configure.sh
+++ ci/configure.sh
@@ -3,6 +3,6 @@
 set -eu
 
 fly -t production set-pipeline \
-  -p bosh:stemcells -c ci/pipeline.yml \
+  -p bosh:stemcells:3421.x -c ci/pipeline.yml \
   -l <(lpass show --note "concourse:production pipeline:bosh:stemcells:3421.x") \
   -l <(lpass show --note "bats-concourse-pool:vsphere secrets")
