bb12bb14573696e17bba65cfb485b74e83fc13e1
Danny Berger <dberger@pivotal.io>
2017-08-03 12:38:49 -0700

    Branching for the 3431.x stemcell series.
    [#149936513](https://www.pivotaltracker.com/story/show/149936513)
    
    Signed-off-by: Dale Wick <dwick@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -3,6 +3,6 @@
 set -eu
 
 fly -t production set-pipeline \
-  -p bosh:stemcells:3421.x -c ci/pipeline.yml \
-  -l <(lpass show --note "concourse:production pipeline:bosh:stemcells:3421.x") \
+  -p bosh:stemcells:3431.x -c ci/pipeline.yml \
+  -l <(lpass show --note "concourse:production pipeline:bosh:stemcells:3431.x") \
   -l <(lpass show --note "bats-concourse-pool:vsphere secrets")
