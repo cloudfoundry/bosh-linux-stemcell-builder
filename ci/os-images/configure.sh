fbc0bac4c0f405fafc4fe4bf846adec93022e056
James Young <jayoung@pivotal.io>
2017-08-14 10:02:53 -0700

    Creating 3445 stemcell line
    [#150179423](https://www.pivotaltracker.com/story/show/150179423)
    
    Signed-off-by: Tom Viehman <tviehman@pivotal.io>

--- ci/os-images/configure.sh
+++ ci/os-images/configure.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
-fly -t production set-pipeline -p bosh:os-image \
+STEMCELL_VERSION=3445.x
+
+fly -t production set-pipeline -p bosh:os-image:$STEMCELL_VERSION \
     -c ci/os-images/pipeline.yml \
-    --load-vars-from <(lpass show -G "concourse:production pipeline:os-images" --notes)
+    --load-vars-from <(lpass show -G "concourse:production pipeline:os-images:$STEMCELL_VERSION" --notes)
