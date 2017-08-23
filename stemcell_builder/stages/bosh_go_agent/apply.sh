3a5f69801935ea3be6cb1984b570688459e67ce3
Tyler Schultz <tschultz@pivotal.io>
2017-08-23 15:33:15 -0700

    Upgrade agent to 2.13.0
    - all nats client calls time out after 5min
    
    [#150416160](https://www.pivotaltracker.com/story/show/150416160)
    
    Signed-off-by: Alton Fong <altonf@vmware.com>

--- stemcell_builder/stages/bosh_go_agent/apply.sh
+++ stemcell_builder/stages/bosh_go_agent/apply.sh
@@ -26,11 +26,11 @@ cp -a $assets_dir/alerts.monitrc $chroot/var/vcap/monit/alerts.monitrc
 
 cd $assets_dir
 if is_ppc64le; then
-  curl -L -o bosh-agent "https://s3.amazonaws.com/bosh-agent-binaries/bosh-agent-2.12.0-linux-ppc64le?versionId=AM4_E8cUslVS6WXcQFtQiPqkoZ4y4fuF"
-  echo "2246984be40d5003232a91b356aed876598b3097adbbb943e8e3849dc09067fd  bosh-agent" | shasum -a 256 -c -
+  curl -L -o bosh-agent "https://s3.amazonaws.com/bosh-agent-binaries/bosh-agent-2.13.0-linux-ppc64le?versionId=8oxoogdUEXKtIKhXUUmnY.fV3p0ZoZp."
+  echo "c6babfa3da9c32b6bfecbfdf77225bc800ab9c4319df2139a027dc2fac482d2b  bosh-agent" | shasum -a 256 -c -
 else
-  curl -L -o bosh-agent "https://s3.amazonaws.com/bosh-agent-binaries/bosh-agent-2.12.0-linux-amd64?versionId=u_UnWtKxvnC_EkHqi676OdGPb6UVxud1"
-  echo "5418997df34fd70c6795331db33801eb7cf27b4df4171c8dc9295efedbb6efe2  bosh-agent" | shasum -a 256 -c -
+  curl -L -o bosh-agent "https://s3.amazonaws.com/bosh-agent-binaries/bosh-agent-2.13.0-linux-amd64?versionId=xZ20v.vcSZ7Kw6EVSGiFeLg8mfrV3wv4"
+  echo "86147308cb22af4150eb0fcf3a41e11aeb3c20a6381514f75894a3ef5ae522f3  bosh-agent" | shasum -a 256 -c -
 fi
 mv bosh-agent $chroot/var/vcap/bosh/bin/
 
