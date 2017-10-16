bd335ce18cc570ce5630cf24322077622ddc56c0
ailan.gl <ailan.gl@alibaba-inc.com>
2017-10-16 20:42:44 +0800

    support for Alibaba Cloud. add alicloud-kvm-ubuntu-trusty support
    	new file:   bosh-stemcell/spec/stemcells/alicloud_spec.rb
    	new file:   stemcell_builder/stages/bosh_alicloud_agent_settings/apply.sh
    	modified:   bosh-stemcell/lib/bosh/stemcell/infrastructure.rb
    	modified:   bosh-stemcell/lib/bosh/stemcell/stage_collection.rb

---
+++ stemcell_builder/stages/bosh_alicloud_agent_settings/apply.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+base_dir=$(readlink -nf $(dirname $0)/../..)
+source $base_dir/lib/prelude_apply.bash
+
+cat > $chroot/var/vcap/bosh/agent.json <<JSON
+{
+  "Platform": {
+    "Linux": {
+      "CreatePartitionIfNoEphemeralDisk": true,
+      "DevicePathResolutionType": "virtio"
+    }
+  },
+  "Infrastructure": {
+    "Settings": {
+      "Sources": [
+        {
+          "Type": "HTTP",
+          "URI": "http://100.100.100.200"
+        }
+      ],
+      "UseRegistry": true
+    }
+  }
+}
+JSON
