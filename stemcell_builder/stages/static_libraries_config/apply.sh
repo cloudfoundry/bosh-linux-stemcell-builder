17cf1ebb6d9290ed789c5ef18c3f91c4d30fc091
Difan Zhao <dzhao@pivotal.io>
2017-11-01 11:35:07 -0700

    Add guard code for non ubuntu stemcell
    [#151744820](https://www.pivotaltracker.com/story/show/151744820)
    
    Signed-off-by: Tom Viehman <tviehman@pivotal.io>

--- stemcell_builder/stages/static_libraries_config/apply.sh
+++ stemcell_builder/stages/static_libraries_config/apply.sh
@@ -6,7 +6,7 @@ base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 source $base_dir/lib/prelude_bosh.bash
 
-if [ ${DISTRIB_CODENAME} == 'xenial' ]; then
+if ([ $(get_os_type) == 'ubuntu' ] && [ ${DISTRIB_CODENAME} == 'xenial' ]); then
   cp -p $assets_dir/xenial_static_libraries_list.txt $chroot/var/vcap/bosh/etc/static_libraries_list
 else
   cp -p $assets_dir/static_libraries_list.txt $chroot/var/vcap/bosh/etc/static_libraries_list
