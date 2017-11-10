f3439db3de5d93cf09e12a688d38350aa3d03a8a
Kai Hofstetter <kai.hofstetter@sap.com>
2017-11-09 17:10:10 -0800

    bump bosh agent 2.30.1 -> 2.30.2 for ipv6 support
    fixed wrong versionId for the bosh-agent download links in the ci
    pipeline
    
    [#152702850](https://www.pivotaltracker.com/story/show/152702850)
    
    Signed-off-by: Difan Zhao <dzhao@pivotal.io>

--- stemcell_builder/stages/bosh_go_agent/apply.sh
+++ stemcell_builder/stages/bosh_go_agent/apply.sh
@@ -26,10 +26,10 @@ cp -a $assets_dir/alerts.monitrc $chroot/var/vcap/monit/alerts.monitrc
 
 cd $assets_dir
 if is_ppc64le; then
-  curl -L -o bosh-agent "https://s3.amazonaws.com/bosh-agent-binaries/bosh-agent-2.30.2-linux-ppc64l?versionId=fHekeJG71MM7A68EWO_CzPuOv2PKs3QZ"
+  curl -L -o bosh-agent "https://s3.amazonaws.com/bosh-agent-binaries/bosh-agent-2.30.2-linux-ppc64le?versionId=BYHDmiSttpCtd_CanK2bPd77mPPWKrnr"
   echo "34705d808e93b0f9a1cae7da68d94a7fadb09b420eff7092e7aa89e4397349a5  bosh-agent" | shasum -a 256 -c -
 else
-  curl -L -o bosh-agent "https://s3.amazonaws.com/bosh-agent-binaries/bosh-agent-2.30.2-linux-amd64?versionId=BYHDmiSttpCtd_CanK2bPd77mPPWKrnr"
+  curl -L -o bosh-agent "https://s3.amazonaws.com/bosh-agent-binaries/bosh-agent-2.30.2-linux-amd64?versionId=fHekeJG71MM7A68EWO_CzPuOv2PKs3QZ"
   echo "1cb6c17854757b97fdb546ecc3b8f9d674d2f7b457cf9758b5b17ab9ecdba4f6  bosh-agent" | shasum -a 256 -c -
 fi
 mv bosh-agent $chroot/var/vcap/bosh/bin/
