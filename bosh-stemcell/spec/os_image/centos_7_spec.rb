55f87929a8b90431db6e67aeda433bef8b5638a3
Joshua Aresty <joshua.aresty@emc.com>
2017-11-15 11:22:51 -0800

    Add shared ntpdate spec to centos
    - This fixes some broken builds
    
    [#152702490](https://www.pivotaltracker.com/story/show/152702490)
    
    Signed-off-by: Zachary Gershman <zgershman@pivotal.io>

--- bosh-stemcell/spec/os_image/centos_7_spec.rb
+++ bosh-stemcell/spec/os_image/centos_7_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe 'CentOS 7 OS image', os_image: true do
   it_behaves_like 'every OS image'
+  it_behaves_like 'an os with ntpdate'
   it_behaves_like 'a CentOS or RHEL based OS image'
   it_behaves_like 'a systemd-based OS image'
   it_behaves_like 'a Linux kernel 3.x based OS image'
