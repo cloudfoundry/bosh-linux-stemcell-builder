af1e50177057addf378f8a2ec654c3950d48a755
Tom Viehman <tviehman@pivotal.io>
2017-11-15 16:37:15 -0800

    Add rpm diffing for centos-7 on all IaaSes
    [#152339123](https://www.pivotaltracker.com/story/show/152339123)
    
    Signed-off-by: David Ansari <david.ansari@gmx.de>

--- bosh-stemcell/spec/stemcells/centos_7_spec.rb
+++ bosh-stemcell/spec/stemcells/centos_7_spec.rb
@@ -76,6 +76,65 @@ HERE
       its (:content) { should_not match /nfs/ }
     end
   end
+
+  describe 'installed packages' do
+    rpm_list_packages = 'rpm --query --all --queryformat="%{NAME}\n"'
+
+    let(:rpm_list_centos) { File.readlines(spec_asset('rpm-list-centos-7.txt')).map(&:chop) }
+    let(:rpm_list_google_centos) { File.readlines(spec_asset('rpm-list-centos-7-google-additions.txt')).map(&:chop) }
+    let(:rpm_list_vsphere_centos) { File.readlines(spec_asset('rpm-list-centos-7-vsphere-additions.txt')).map(&:chop) }
+    let(:rpm_list_azure_centos) { File.readlines(spec_asset('rpm-list-centos-7-azure-additions.txt')).map(&:chop) }
+
+    describe command(rpm_list_packages), {
+      exclude_on_google: true,
+      exclude_on_vcloud: true,
+      exclude_on_vsphere: true,
+      exclude_on_azure: true,
+    } do
+      it 'contains only the base set of packages for aws, openstack, warden' do
+        expect(subject.stdout.split("\n")).to match_array(rpm_list_centos)
+      end
+    end
+
+    describe command(rpm_list_packages), {
+      exclude_on_aws: true,
+      exclude_on_vcloud: true,
+      exclude_on_vsphere: true,
+      exclude_on_warden: true,
+      exclude_on_azure: true,
+      exclude_on_openstack: true,
+    } do
+      it 'contains only the base set of packages plus google-specific packages' do
+        expect(subject.stdout.split("\n")).to match_array(rpm_list_centos.concat(rpm_list_google_centos))
+      end
+    end
+
+    describe command(rpm_list_packages), {
+      exclude_on_aws: true,
+      exclude_on_google: true,
+      exclude_on_warden: true,
+      exclude_on_azure: true,
+      exclude_on_openstack: true,
+    } do
+      it 'contains only the base set of packages plus vsphere-specific packages' do
+        expect(subject.stdout.split("\n")).to match_array(rpm_list_centos.concat(rpm_list_vsphere_centos))
+      end
+    end
+
+    describe command(rpm_list_packages), {
+      exclude_on_aws: true,
+      exclude_on_vcloud: true,
+      exclude_on_vsphere: true,
+      exclude_on_google: true,
+      exclude_on_warden: true,
+      exclude_on_openstack: true,
+    } do
+      it 'contains only the base set of packages plus azure-specific packages' do
+        expect(subject.stdout.split("\n")).to match_array(rpm_list_centos.concat(rpm_list_azure_centos))
+      end
+    end
+  end
+
 end
 
 describe 'CentOS 7 stemcell tarball', stemcell_tarball: true do
