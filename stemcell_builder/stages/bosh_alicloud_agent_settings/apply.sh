5d486444a2912d8c89aa2f9ce3c0f8e4e1738690
ailan.gl <ailan.gl@alibaba-inc.com>
2017-12-13 14:33:29 +0800

    enable public key fetch

--- stemcell_builder/stages/bosh_alicloud_agent_settings/apply.sh
+++ stemcell_builder/stages/bosh_alicloud_agent_settings/apply.sh
@@ -3,6 +3,8 @@
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
+KEY_ID=`curl http://100.100.100.200/latest/meta-data/public-keys/`
+
 cat > $chroot/var/vcap/bosh/agent.json <<JSON
 {
   "Platform": {
@@ -18,6 +20,7 @@ cat > $chroot/var/vcap/bosh/agent.json <<JSON
           "Type": "HTTP",
           "URI": "http://100.100.100.200",
           "InstanceIDPath": "/latest/meta-data/instance-id",
+          "SSHKeysPath": "/latest/meta-data/public-keys/$KEY_ID/openssh-key",
           "UserDataPath": "/latest/user-data"
         }
       ],
