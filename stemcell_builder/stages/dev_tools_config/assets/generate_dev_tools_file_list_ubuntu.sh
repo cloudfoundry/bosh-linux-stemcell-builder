c6d6ed6a4c3990f3e65f898bf841400b84dbb05c
Luan Santos <lsantos@pivotal.io>
2018-01-03 13:36:15 -0800

    Make dev-tools packages removal more explicit by package name
    [#153391129](https://www.pivotaltracker.com/story/show/153391129)

--- stemcell_builder/stages/dev_tools_config/assets/generate_dev_tools_file_list_ubuntu.sh
+++ stemcell_builder/stages/dev_tools_config/assets/generate_dev_tools_file_list_ubuntu.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-for package_name in \
+declare -a PACKAGES
+
+PACKAGES=( \
   binutils \
   bison \
   build-essential \
@@ -18,6 +20,14 @@ for package_name in \
   make \
   patch \
   po-debconf \
-; do
-  (for i in `dpkg-query -W -f '${Package} ' ${package_name}*`; do dpkg -L $i; done) | xargs file | grep -Ev ':\s+directory\s*$' | awk -F ':' '{ print $1 }'
+)
+
+if [ "$1" == 'xenial' ]; then
+  PACKAGES+=(cpp-5 g++-5 gcc-5 gcc-5-base gcc-6-base)
+else
+  PACKAGES+=(cpp-4.8 g++-4.8 gcc-4.8)
+fi
+
+for package_name in ${PACKAGES[*]} ; do
+  dpkg-query -L "$package_name" | xargs file | grep -Ev ':\s+directory\s+$' | awk -F ':' '{ print $1 }'
 done
