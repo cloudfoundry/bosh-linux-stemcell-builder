de77af6929dc48a07d7c8e3db6d7b92b78285730
J Evelyn Quintana <jquintana@pivotal.io>
2018-01-17 11:02:09 -0800

    assert that sync-time works
    and fix trusty version of the script
    
    [#154294704](https://www.pivotaltracker.com/story/show/154294704)
    
    Signed-off-by: Joshua Aresty <joshua.aresty@emc.com>

--- src/github.com/cloudfoundry/stemcell-acceptance-tests/ipv4director/smoke/smoke_test.go
+++ src/github.com/cloudfoundry/stemcell-acceptance-tests/ipv4director/smoke/smoke_test.go
@@ -3,6 +3,7 @@ package smoke_test
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -136,30 +137,71 @@ var _ = Describe("Stemcell", func() {
 		Expect(stdout).To(ContainSubstring("eth0"))
 	})
 
-	It("corrects systemtime via chrony", func() {
-		if os.Getenv("BOSH_os_name") != "ubuntu-xenial" {
-			Skip(`please set BOSH_os_name to "ubuntu-xenial" run this test`)
-		}
+	Context("when synchronizing the clock on the instance via ntp", func() {
+		It("corrects xenial systemtime via chrony", func() {
+			if os.Getenv("BOSH_os_name") != "ubuntu-xenial" {
+				Skip(`please set BOSH_os_name to "ubuntu-xenial" run this test`)
+			}
 
-		stdout, _, exitStatus, err := bosh.Run(
-			"--column=stdout",
-			"ssh", "default/0", "-r", "-c",
-			`sudo chronyc -a tracking`,
-		)
-		Expect(err).ToNot(HaveOccurred())
-		Expect(exitStatus).To(Equal(0))
+			stdout, _, exitStatus, err := bosh.Run(
+				"--column=stdout",
+				"ssh", "default/0", "-r", "-c",
+				`sudo chronyc -a tracking`,
+			)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(exitStatus).To(Equal(0))
 
-		ntpServer := regexp.MustCompile(`Reference ID\s+:(\s[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})`)
-		match := ntpServer.FindAllStringSubmatch(stdout, -1)
-		Expect(match[0][1]).NotTo(Equal("0.0.0.0"))
+			ntpServer := regexp.MustCompile(`Reference ID\s+:(\s[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})`)
+			match := ntpServer.FindAllStringSubmatch(stdout, -1)
+			Expect(match[0][1]).NotTo(Equal("0.0.0.0"))
 
-		systemTime := regexp.MustCompile(`System time\s+:\s(\d\.\d+)`)
-		match = systemTime.FindAllStringSubmatch(stdout, -1)
+			systemTime := regexp.MustCompile(`System time\s+:\s(\d\.\d+)`)
+			match = systemTime.FindAllStringSubmatch(stdout, -1)
 
-		drift, err := strconv.ParseFloat(match[0][1], 32)
-		Expect(err).NotTo(HaveOccurred())
+			drift, err := strconv.ParseFloat(match[0][1], 32)
+			Expect(err).NotTo(HaveOccurred())
 
-		Expect(drift).To(BeNumerically("<", 1))
+			Expect(drift).To(BeNumerically("<", 1))
+
+			By("running the sync-time script, we do not see an error", func() {
+				_, _, exitStatus, err := bosh.Run(
+					"--column=stdout",
+					"ssh", "default/0", "-r", "-c",
+					`sudo /var/vcap/bosh/bin/sync-time`,
+				)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(exitStatus).To(Equal(0))
+			})
+		})
+
+		It("corrects trusty systemtime via ntpdate", func() {
+			if os.Getenv("BOSH_os_name") != "ubuntu-trusty" {
+				Skip(`please set BOSH_os_name to "ubuntu-trusty" run this test`)
+			}
+
+			stdout, _, exitStatus, err := bosh.Run(
+				"--column=stdout",
+				"ssh", "default/0", "-r", "-c",
+				`sudo bash -c "ntpdate -q $(sudo cat /var/vcap/bosh/etc/ntpserver) | tail -1 | sed -E 's/^.* offset ([-0-9.]+) .*$/\1/'"`,
+			)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(exitStatus).To(Equal(0))
+
+			drift, err := strconv.ParseFloat(strings.TrimSpace(stdout), 64)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(math.Abs(drift)).To(BeNumerically("<", 1))
+
+			By("running the sync-time script, we do not see an error", func() {
+				_, _, exitStatus, err := bosh.Run(
+					"--column=stdout",
+					"ssh", "default/0", "-r", "-c",
+					`sudo /var/vcap/bosh/bin/sync-time`,
+				)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(exitStatus).To(Equal(0))
+			})
+		})
 	})
 
 	It("#153391129: removes dev tools and static libraries", func() {
@@ -246,4 +288,4 @@ var _ = Describe("Stemcell", func() {
 		})
 	})
 
-	})
+})
