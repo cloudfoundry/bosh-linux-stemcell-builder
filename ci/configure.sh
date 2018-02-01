7ddfe335c8fa28978028f26a14be2e857178598a
Danny Berger <dberger@pivotal.io>
2018-02-01 14:34:11 -0800

    Break out stemcell publishing functionality
    Removes relevant resources and jobs for publishing stemcells into an ops
    file that can be interpolated and used for adding that functionality to
    branches as necessary.
    
    [#154855061](https://www.pivotaltracker.com/story/show/154855061)
    
    Signed-off-by: Josh Russett <jrussett@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -2,7 +2,12 @@
 
 set -eu
 
+args=""
+
+# uncomment the following on release branches
+#args="-o ci/release-branch-pipeline-ops.yml"
+
 fly -t production set-pipeline \
-  -p bosh:stemcells -c ci/pipeline.yml \
+  -p bosh:stemcells -c <( bosh interpolate $args ci/pipeline.yml ) \
   -l <(lpass show --note "concourse:production pipeline:bosh:stemcells") \
   -l <(lpass show --note "bats-concourse-pool:vsphere secrets")
