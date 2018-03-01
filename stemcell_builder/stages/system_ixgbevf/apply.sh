0e0c19d293df6b4910a9d359639e96f31e4fdc69
Marco Voelz <marco.voelz@sap.com>
2018-03-01 11:40:24 +0100

    Bump ixgbevf to 4.3.4

--- stemcell_builder/stages/system_ixgbevf/apply.sh
+++ stemcell_builder/stages/system_ixgbevf/apply.sh
@@ -6,20 +6,20 @@ base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 source $base_dir/etc/settings.bash
 
-mkdir $chroot/usr/src/ixgbevf-4.2.2
+mkdir $chroot/usr/src/ixgbevf-4.3.4
 
-tar -xzf $assets_dir/ixgbevf-4.2.2.tar.gz \
-  -C $chroot/usr/src/ixgbevf-4.2.2 \
+tar -xzf $assets_dir/ixgbevf-4.3.4.tar.gz \
+  -C $chroot/usr/src/ixgbevf-4.3.4 \
   --strip-components=1
 
-cp $assets_dir/usr/src/ixgbevf-4.2.2/dkms.conf $chroot/usr/src/ixgbevf-4.2.2/dkms.conf
+cp $assets_dir/usr/src/ixgbevf-4.3.4/dkms.conf $chroot/usr/src/ixgbevf-4.3.4/dkms.conf
 
 pkg_mgr install dkms
 
 kernelver=$( ls -rt $chroot/lib/modules | tail -1 )
-run_in_chroot $chroot "dkms -k ${kernelver} add -m ixgbevf -v 4.2.2"
-run_in_chroot $chroot "dkms -k ${kernelver} build -m ixgbevf -v 4.2.2"
-run_in_chroot $chroot "dkms -k ${kernelver} install -m ixgbevf -v 4.2.2"
+run_in_chroot $chroot "dkms -k ${kernelver} add -m ixgbevf -v 4.3.4"
+run_in_chroot $chroot "dkms -k ${kernelver} build -m ixgbevf -v 4.3.4"
+run_in_chroot $chroot "dkms -k ${kernelver} install -m ixgbevf -v 4.3.4"
 
 
 if [ -f ${chroot}/etc/debian_version ] # Ubuntu
