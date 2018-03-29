91d886d3eee647c8c51cf076204654ffa40de61b
Chris Dutra <cdutra@pivotal.io>
2018-03-29 12:26:48 -0700

    Auto-deliver stories
    [#156316653](https://www.pivotaltracker.com/story/show/156316653)
    
    Signed-off-by: Michael Xu <mxu@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -5,4 +5,5 @@ set -eu
 fly -t production set-pipeline \
   -p bosh:stemcells:3421.x -c ci/pipeline.yml \
   -l <(lpass show --note "concourse:production pipeline:bosh:stemcells:3421.x") \
-  -l <(lpass show --note "bats-concourse-pool:vsphere secrets")
+  -l <(lpass show --note "bats-concourse-pool:vsphere secrets") \
+  -l <(lpass show --note "tracker-bot-story-delivery")
