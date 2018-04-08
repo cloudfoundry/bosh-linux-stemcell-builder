56e1d692dc3fda9559362b153a16e3be81d374cc
gossion <guwe@microsoft.com>
2018-04-08 05:59:23 +0000

    Support Azure serial console

---
+++ stemcell_builder/stages/system_azure_serial_console/apply.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+base_dir=$(readlink -nf $(dirname $0)/../..)
+source $base_dir/lib/prelude_apply.bash
+
+if [ "$(get_os_type)" == "ubuntu" ]; then
+  cat >> $chroot/etc/init/ttyS0.conf <<EOS
+# ttyS0 - getty
+#
+# This service maintains a getty on ttyS0 from the point the system is
+# started until it is shut down again.
+
+start on stopped rc RUNLEVEL=[12345]
+stop on runlevel [!12345]
+
+pre-start script
+    # getty will not be started if the serial console is not present
+    stty -F /dev/ttyS0 -a 2> /dev/null > /dev/null || { stop; exit 0; }
+end script
+
+respawn
+script
+    exec /sbin/getty -L ttyS0 115200 vt102
+end script
+EOS
+fi
