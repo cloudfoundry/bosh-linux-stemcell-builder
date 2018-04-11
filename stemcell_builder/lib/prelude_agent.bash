1beafdc9b926815a71394547a5f09fb1947289db
Babu Prasad Elumalai <babupe@google.com>
2018-04-11 01:45:48 +0000

    https://github.com/cloudfoundry/bosh/issues/1722

--- stemcell_builder/lib/prelude_agent.bash
+++ stemcell_builder/lib/prelude_agent.bash
@@ -6,3 +6,11 @@ function get_partitioner_type_mapping {
       echo ''
   fi
 }
+
+function get_google_partitioner_type_mapping {
+  if [ "$(get_os_type)" == "opensuse" ] || ( [ "$(get_os_type)" == "ubuntu" ] && [ "${DISTRIB_CODENAME}" == "xenial" ]) || ( [ "$(get_os_type)" == "ubuntu" ] && [ "${DISTRIB_CODENAME}" == "trusty" ]); then
+      echo '"PartitionerType": "parted",'
+  else
+      echo ''
+  fi
+}
