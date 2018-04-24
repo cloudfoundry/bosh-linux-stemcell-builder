6bc387229c60c0c074f2d7e2c1bed1134ddbba15
Chris Dutra <cdutra@pivotal.io>
2018-04-24 12:11:37 -0700

    Use vSphere 6.5 environment
    - use vSphere resource pools
    
    [#156854395](https://www.pivotaltracker.com/story/show/156854395)
    
    Signed-off-by: Rob Day-Reynolds <rdayreynolds@pivotal.io>

--- ci/tasks/deploy-director.sh
+++ ci/tasks/deploy-director.sh
@@ -26,6 +26,7 @@ chmod +x $bosh_cli
 
 $bosh_cli interpolate bosh-deployment/bosh.yml \
   -o bosh-deployment/vsphere/cpi.yml \
+  -o bosh-deployment/vsphere/resource-pool.yml \
   --vars-store director-creds.yml \
   -v director_name=stemcell-smoke-tests-director \
   --vars-env "BOSH" > director.yml
