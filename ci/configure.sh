09c50cdeb7b189a051a84792a9e5b34edaf6eed9
Chris Dutra <cdutra@pivotal.io>
2018-04-27 11:48:07 -0700

    Remove deprecated bosh:os-image:3468.x
    Signed-off-by: Josh Russett <jrussett@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -5,6 +5,7 @@ set -eu
 fly -t production set-pipeline \
   -p bosh:stemcells:3468.x -c ci/pipeline.yml \
   -l <(lpass show --note "concourse:production pipeline:bosh:stemcells") \
+  -l <(lpass show "concourse:production pipeline:os-images" --notes) \
   -l <(lpass show --note "bats-concourse-pool:vsphere secrets") \
   -l <(lpass show --note "tracker-bot-story-delivery") \
   -v stemcell_branch=3468.x \
