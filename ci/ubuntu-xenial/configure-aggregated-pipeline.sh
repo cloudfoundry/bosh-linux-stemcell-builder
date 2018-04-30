3abd46f7be7abd48a6a21c6e205619b1f0b575fa
Michael Xu <mxu@pivotal.io>
2018-04-30 15:59:33 -0700

    Make ubuntu-xenial version bump configurable
    [#156625971](https://www.pivotaltracker.com/story/show/156625971)
    
    Signed-off-by: Danny Berger <dberger@pivotal.io>

--- ci/ubuntu-xenial/configure-aggregated-pipeline.sh
+++ ci/ubuntu-xenial/configure-aggregated-pipeline.sh
@@ -8,8 +8,8 @@ fly -t production set-pipeline \
   -p "bosh:stemcells:ubuntu-xenial" \
   -c <(
     bosh interpolate \
-      -o <( bosh int -v group=master -v branch=master            -v initial_version=0.0.0  $dir/pipeline-branch-ops.yml ) \
-      -o <( bosh int -v group=1.x    -v branch=ubuntu-xenial/1.x -v initial_version=1.98.0 <( git show ubuntu-xenial/1.x:ci/ubuntu-xenial/pipeline-branch-ops.yml ) ) \
+      -o <( bosh int -v group=master -v branch=master            -v initial_version=0.0.0  -v bump_version=major $dir/pipeline-branch-ops.yml ) \
+      -o <( bosh int -v group=1.x    -v branch=ubuntu-xenial/1.x -v initial_version=1.98.0 -v bump_version=minor <( git show ubuntu-xenial/1.x:ci/ubuntu-xenial/pipeline-branch-ops.yml ) ) \
       $dir/pipeline-base.yml
   ) \
   -l <( lpass show --note "concourse:production pipeline:os-images" ) \
