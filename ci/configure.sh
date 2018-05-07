6ff558e50790fc63301aa322ef5258b1b0096fbc
Luan Santos <lsantos@pivotal.io>
2018-05-07 14:33:52 -0700

    Update logic for triggering images every 2 weeks
    This change updates the time resource to trigger every 8 days on Mondays
    only. By triggering on an interval of 8 days, we remove the ambiguity of
    triggering every 14 days where you could potentially miss a period if
    the check falls out of the trigger start/stop range.
    
    [finishes #157364256](https://www.pivotaltracker.com/story/show/157364256)
    
    Co-authored-by: James Myers <jmyers@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -2,13 +2,20 @@
 
 set -eu
 
+
+absolute_path() {
+  (cd "$1" && pwd)
+}
+
+scripts_path=$(absolute_path "$(dirname "$0")" )
+
 pipeline="bosh:stemcells"
 args=""
 
 if [ -n "${RELEASE_BRANCH:-}" ]; then
   pipeline="$pipeline:$RELEASE_BRANCH"
   args="
-    -o ci/release-branch-pipeline-ops.yml
+    -o ${scripts_path}/release-branch-pipeline-ops.yml
     -v release_branch=$RELEASE_BRANCH
     -v initial_version=${RELEASE_BRANCH/.x/.1.0}
   "
@@ -16,7 +23,7 @@ fi
 
 fly -t production set-pipeline \
   -p "$pipeline" \
-  -c <( bosh interpolate $args ci/pipeline.yml ) \
+  -c <(bosh interpolate $args ${scripts_path}/pipeline.yml) \
   -l <(lpass show --note "concourse:production pipeline:bosh:stemcells") \
   -l <(lpass show --notes "concourse:production pipeline:os-images") \
   -l <(lpass show --note "bats-concourse-pool:vsphere secrets") \
