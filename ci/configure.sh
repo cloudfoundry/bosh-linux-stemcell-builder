304368cc01bdb7de7016fafe98d5c32e7caa87ae
Luan Santos <lsantos@pivotal.io>
2018-05-07 11:05:13 -0700

    Update logic for triggering images every 2 weeks
    This change updates the time resource to trigger every 8 days on Mondays
    only. By triggering on an interval of 8 days, we remove the ambiguity of
    triggering every 14 days where you could potentially miss a period if
    the check falls out of the trigger start/stop range.
    
    [finishes #157364252](https://www.pivotaltracker.com/story/show/157364252)
    
    Co-authored-by: James Myers <jmyers@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -2,12 +2,20 @@
 
 set -eu
 
+absolute_path() {
+  (cd "$1" && pwd)
+}
+
+scripts_path=$(absolute_path "$(dirname "$0")")
+
+STEMCELL_VERSION=3468.x
+
 fly -t production set-pipeline \
-  -p bosh:stemcells:3468.x -c ci/pipeline.yml \
+  -p bosh:stemcells:$STEMCELL_VERSION -c "${scripts_path}/pipeline.yml" \
   -l <(lpass show --note "concourse:production pipeline:bosh:stemcells") \
   -l <(lpass show "concourse:production pipeline:os-images" --notes) \
   -l <(lpass show --note "bats-concourse-pool:vsphere secrets") \
   -l <(lpass show --note "tracker-bot-story-delivery") \
-  -v stemcell_branch=3468.x \
-  -v stemcell_version_key=bosh-stemcell/version-3468.x \
+  -v stemcell_branch=$STEMCELL_VERSION \
+  -v stemcell_version_key=bosh-stemcell/version-$STEMCELL_VERSION \
   -v stemcell_version_semver_bump=minor
