8f7ecc21c0b162a9f1acd847d41eaa98954ad342
Danny Berger <dberger@pivotal.io>
2018-06-07 17:14:57 -0700

    Backport: Story creation for periodic rebuilds
    a538e53 Introduce branch variable for create-story
    02eabaf Add job which periodically creates tracker stories
    
    [#158080716](https://www.pivotaltracker.com/story/show/158080716)
    
    Co-authored-by: Max Becker <max.becker@sap.com>

--- ci/configure.sh
+++ ci/configure.sh
@@ -12,10 +12,11 @@ STEMCELL_VERSION=3468.x
 
 fly -t production set-pipeline \
   -p bosh:stemcells:$STEMCELL_VERSION -c "${scripts_path}/pipeline.yml" \
-  -l <(lpass show --note "concourse:production pipeline:bosh:stemcells") \
-  -l <(lpass show "concourse:production pipeline:os-images" --notes) \
-  -l <(lpass show --note "bats-concourse-pool:vsphere secrets") \
-  -l <(lpass show --note "tracker-bot-story-delivery") \
+  -l <(lpass show --notes "concourse:production pipeline:bosh:stemcells") \
+  -l <(lpass show --notes "concourse:production pipeline:os-images") \
+  -l <(lpass show --notes "bats-concourse-pool:vsphere secrets") \
+  -l <(lpass show --notes "tracker-bot-story-delivery") \
+  -l <(lpass show --notes "stemcell-reminder-bot") \
   -v stemcell_branch=$STEMCELL_VERSION \
   -v stemcell_version_key=bosh-stemcell/version-$STEMCELL_VERSION \
   -v stemcell_version_semver_bump=minor
