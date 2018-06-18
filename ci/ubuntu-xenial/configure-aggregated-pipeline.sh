07a545735d08bf0cc028ce8706f52515ab06504f
James Myers <jmyers@pivotal.io>
2018-06-18 10:59:44 -0700

    Remove 1.x pipeline group for ubuntu-xenial
    Co-authored-by: Andrew Gilbert <agilbert@pivotal.io>

--- ci/ubuntu-xenial/configure-aggregated-pipeline.sh
+++ ci/ubuntu-xenial/configure-aggregated-pipeline.sh
@@ -10,14 +10,12 @@ if [[ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]]; then
 fi
 
 git fetch --all
-git branch --track ubuntu-xenial/1.x origin/ubuntu-xenial/1.x 2>/dev/null || true
 
 fly -t production set-pipeline \
   -p "bosh:stemcells:ubuntu-xenial" \
   -c <(
     bosh interpolate \
       -o <( bosh int -v group=master -v branch=master            -v initial_version=0.0.0  -v bump_version=major $dir/pipeline-branch-ops.yml ) \
-      -o <( bosh int -v group=1.x    -v branch=ubuntu-xenial/1.x -v initial_version=1.98.0 -v bump_version=minor <( git show ubuntu-xenial/1.x:ci/ubuntu-xenial/pipeline-branch-ops.yml ) ) \
       $dir/pipeline-base.yml
   ) \
   -l <( lpass show --notes "concourse:production pipeline:os-images" ) \
