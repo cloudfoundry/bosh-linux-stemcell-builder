487bed46ecde50f574903cb54fa0b113dde8803e
Chris Dutra <cdutra@pivotal.io>
2018-07-10 11:28:57 -0700

    Set chrony waitsync max retires to 10
    [GH: #54]
    [finishes #158948148](https://www.pivotaltracker.com/story/show/158948148)
    
    Co-authored-by: Luan Santos <lsantos@pivotal.io>

--- bosh-stemcell/spec/support/os_image_chrony_shared_examples.rb
+++ bosh-stemcell/spec/support/os_image_chrony_shared_examples.rb
@@ -3,6 +3,7 @@ shared_examples_for 'an os with chrony' do
     describe file('/var/vcap/bosh/bin/sync-time') do
       it { should be_file }
       its(:content) { should match(/systemctl restart chrony\.service/) }
+      its(:content) { should match(/chronyc waitsync 10/) }
     end
 
     describe file('/etc/chrony/chrony.conf') do
