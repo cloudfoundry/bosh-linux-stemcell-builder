587fc8565fb46f9c726b676bc90959a9c2814054
Joshua Aresty <joshua.aresty@emc.com>
2018-07-10 14:50:54 -0700

    Add 87.x xenial ops
    Signed-off-by: Josh Russett <jrussett@pivotal.io>

--- ci/ubuntu-xenial/configure-aggregated-pipeline.sh
+++ ci/ubuntu-xenial/configure-aggregated-pipeline.sh
@@ -10,12 +10,14 @@ if [[ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]]; then
 fi
 
 git fetch --all
+git branch --track ubuntu-xenial/87.x origin/ubuntu-xenial/87.x 2>/dev/null || true
 
 fly -t production set-pipeline \
   -p "bosh:stemcells:ubuntu-xenial" \
   -c <(
     bosh interpolate \
       -o <( bosh int -v group=master -v branch=master            -v initial_version=0.0.0  -v bump_version=major $dir/pipeline-branch-ops.yml ) \
+      -o <( bosh int -v group=87.x   -v branch=ubuntu-xenial/87.x -v initial_version=87.0.0 -v bump_version=minor <( git show ubuntu-xenial/87.x:ci/ubuntu-xenial/pipeline-branch-ops.yml ) ) \
       $dir/pipeline-base.yml
   ) \
   -l <( lpass show --notes "concourse:production pipeline:os-images" ) \
