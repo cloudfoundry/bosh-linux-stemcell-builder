996aa3c2be6062f26ce52429b91f44aa2b8b3fc4
Chris Dutra <cdutra@pivotal.io>
2018-08-08 14:00:45 -0700

    Split pipeline ops into base and branch
    Base should not contain a publish job. Publish is exclusive to released
    branches.
    
    Co-authored-by: Luan Santos <lsantos@pivotal.io>
    Signed-off-by: Luan Santos <lsantos@pivotal.io>

--- ci/ubuntu-xenial/configure-aggregated-pipeline.sh
+++ ci/ubuntu-xenial/configure-aggregated-pipeline.sh
@@ -16,7 +16,8 @@ fly -t production set-pipeline \
   -p "bosh:stemcells:ubuntu-xenial" \
   -c <(
     bosh interpolate \
-      -o <( bosh int -v group=master -v branch=master            -v initial_version=0.0.0  -v bump_version=major $dir/pipeline-branch-ops.yml ) \
+      -o <( bosh int -v group=master -v branch=master             -v initial_version=0.0.0  -v bump_version=major $dir/pipeline-base-ops.yml ) \
+      -o <( bosh int -v group=97.x   -v branch=ubuntu-xenial/97.x -v initial_version=97.0.0 -v bump_version=minor <( git show ubuntu-xenial/97.x:ci/ubuntu-xenial/pipeline-base-ops.yml ) ) \
       -o <( bosh int -v group=97.x   -v branch=ubuntu-xenial/97.x -v initial_version=97.0.0 -v bump_version=minor <( git show ubuntu-xenial/97.x:ci/ubuntu-xenial/pipeline-branch-ops.yml ) ) \
       $dir/pipeline-base.yml
   ) \
