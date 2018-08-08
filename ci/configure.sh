560eba853e66d798cfa1e7808101ab810deb94f1
Chris Dutra <cdutra@pivotal.io>
2018-08-08 12:04:02 -0700

    Consolidate into one lastpass note
    Co-authored-by: Luan Santos <lsantos@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -12,8 +12,8 @@ STEMCELL_VERSION=3421.x
 
 fly -t production set-pipeline \
   -p bosh:stemcells:$STEMCELL_VERSION -c "${scripts_path}/pipeline.yml" \
-  -l <(lpass show --notes "concourse:production pipeline:bosh:stemcells:$STEMCELL_VERSION") \
-  -l <(lpass show -G --notes "concourse:production pipeline:os-images:$STEMCELL_VERSION") \
+  -l <(lpass show --notes "concourse:production pipeline:bosh:stemcells") \
+  -l <(lpass show --notes "concourse:production pipeline:os-images") \
   -l <(lpass show --notes "bats-concourse-pool:vsphere secrets") \
   -l <(lpass show --notes "tracker-bot-story-delivery") \
   -l <(lpass show --notes "stemcell-reminder-bot")
