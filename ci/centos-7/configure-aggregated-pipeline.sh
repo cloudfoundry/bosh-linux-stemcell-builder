dd3f364c97ff79a48fdcd7ae8a61e25608150d63
Danny Berger <dberger@pivotal.io>
2018-11-19 14:20:30 -0800

    Split centos-7 stemcells into a new dedicated pipeline
    This change moves centos-7 stemcell production into a new centos 7
    pipeline similar to the ubuntu-xenial line. Any new versions of the
    centos 7 stemcell should be produced from this pipeline in a pipeline
    group.
    
    This change also removes the outdated master pipeline that formerly
    produced trusty/centos stemcells. Stemcells are now split into a OS and
    version specific pipeline that produces for all relevant versions.
    
    [finishes #162072036](https://www.pivotaltracker.com/story/show/162072036)
    
    Co-authored-by: James Myers <jmyers@pivotal.io>

---
+++ ci/centos-7/configure-aggregated-pipeline.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+dir=$(dirname $0)
+
+if [[ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]]; then
+  echo -e "Do not run this script from any branch other than \033[1mmaster\033[0m"
+  exit 1
+fi
+
+git fetch --all
+
+fly -t production set-pipeline \
+  -p "bosh:stemcells:centos-7" \
+  -c <(
+    bosh interpolate \
+      -o <( bosh int -v group=master -v branch=master -v initial_version="3728.0.0" -v bump_version=major $dir/pipeline-base-ops.yml ) \
+      $dir/pipeline-base.yml
+  ) \
+  -l <( lpass show --notes "concourse:production pipeline:os-images" ) \
+  -l <( lpass show --notes "concourse:production pipeline:bosh:stemcells" ) \
+  -l <( lpass show --notes "bats-concourse-pool:vsphere secrets" ) \
+  -l <( lpass show --notes "tracker-bot-story-delivery" ) \
+  -l <(lpass show --notes "stemcell-reminder-bot")
