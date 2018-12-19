2431b52e08eb4a78bf817e886d575051e567668b
Belinda Liu <bliu@pivotal.io>
2018-12-19 14:00:06 -0800

    Merge logrotate scripts into one
    [#162399779](https://www.pivotaltracker.com/story/show/162399779)
    
    Co-authored-by: Joshua Aresty <joshua.aresty@emc.com>

--- stemcell_builder/stages/logrotate_config/assets/setup-logrotate.sh
+++ stemcell_builder/stages/logrotate_config/assets/setup-logrotate.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-print-logrotate-cron.sh $RANDOM > /etc/cron.d/logrotate
+: "${firstMinute:=$((RANDOM % 15))}"
+
+firstMinute=$(($1 % 15))
+secondMinute=$((firstMinute+15))
+thirdMinute=$((secondMinute+15))
+fourthMinute=$((thirdMinute+15))
+
+echo "$firstMinute,$secondMinute,$thirdMinute,$fourthMinute * * * * root /usr/bin/logrotate-cron" > /etc/cron.d/logrotate
 
 touch /etc/cron.d
