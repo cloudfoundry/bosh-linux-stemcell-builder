a643e194f3f32bfb4fae850eda8bf511cb5c75b2
Danny Berger <dberger@pivotal.io>
2019-01-16 14:27:56 -0500

    Update centos-7 pipeline for major line 3763.x
    [#162818029](https://www.pivotaltracker.com/story/show/162818029)
    
    Co-authored-by: Gaurab Dey <gdey@pivotal.io>
    Co-authored-by: Andrew Su <asu@pivotal.io>

--- ci/centos-7/configure-aggregated-pipeline.sh
+++ ci/centos-7/configure-aggregated-pipeline.sh
@@ -10,12 +10,14 @@ if [[ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]]; then
 fi
 
 git fetch --all
+git branch --track centos-7/3763.x origin/centos-7/3763.x 2>/dev/null || true
 
 fly -t production set-pipeline \
   -p "bosh:stemcells:centos-7" \
   -c <(
     bosh interpolate \
       -o <( bosh int -v group=master -v branch=master -v initial_version="3728.0.0" -v bump_version=major $dir/pipeline-base-ops.yml ) \
+      -o <( bosh int -v group=3763.x -v branch=centos-7/3763.x -v initial_version="3763.0.0" -v bump_version=minor <( git show centos-7/3763.x:ci/centos-7/pipeline-base-ops.yml ; git show centos-7/3763.x:ci/centos-7/pipeline-branch-ops.yml ) ) \
       $dir/pipeline-base.yml
   ) \
   -l <( lpass show --notes "concourse:production pipeline:os-images" ) \
