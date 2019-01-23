a7c20f67d1e683b2053ac3a9d28e39b3f87e6099
Luan Santos <lsantos@pivotal.io>
2019-01-23 11:16:49 -0800

    Use tarball to trigger/bump and use new tracker
    Co-authored-by: Joshua Aresty <joshua.aresty@emc.com>
    Co-authored-by: Max Petersen <mpetersen@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -21,11 +21,11 @@ if [ -n "${RELEASE_BRANCH:-}" ]; then
   "
 fi
 
+echo $pipeline
 fly -t production set-pipeline \
   -p "$pipeline" \
   -c <(bosh interpolate $args ${scripts_path}/pipeline.yml) \
   -l <(lpass show --note "concourse:production pipeline:bosh:stemcells") \
   -l <(lpass show --notes "concourse:production pipeline:os-images") \
   -l <(lpass show --note "bats-concourse-pool:vsphere secrets") \
-  -l <(lpass show --note "tracker-bot-story-delivery") \
   -l <(lpass show --note "stemcell-reminder-bot")
