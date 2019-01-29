4ed32b00c3a9c387ddb42733a77221485da09496
Joshua Aresty <joshua.aresty@emc.com>
2019-01-28 16:42:59 -0800

    Make bump-agent succeed when there are no changes
    [#163545344](https://www.pivotaltracker.com/story/show/163545344)
    
    Co-authored-by: Max Petersen <mpetersen@pivotal.io>

--- ci/tasks/bump-bosh-agent.sh
+++ ci/tasks/bump-bosh-agent.sh
@@ -8,8 +8,10 @@ cp bosh-agent/.resource/metalink.meta4 bosh-linux-stemcell-builder-out/stemcell_
 cp bosh-agent/.resource/version bosh-linux-stemcell-builder-out/stemcell_builder/stages/bosh_go_agent/assets/bosh-agent-version
 
 pushd bosh-linux-stemcell-builder-out
-  git add -A
-  git config --global user.email "ci@localhost"
-  git config --global user.name "CI Bot"
-  git commit -m "bump bosh-agent/$version"
+	if [ "$(git status --porcelain)" != "" ]; then
+		git add -A
+		git config --global user.email "ci@localhost"
+		git config --global user.name "CI Bot"
+		git commit -m "bump bosh-agent/$version"
+	fi
 popd
