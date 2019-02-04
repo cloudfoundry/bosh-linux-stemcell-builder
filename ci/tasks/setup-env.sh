f86eab6d58b351ab11ebdc9ee27a167bde59eb69
Joshua Aresty <joshua.aresty@emc.com>
2019-02-04 15:10:04 -0800

    Setup acceptance env
    [#163704556](https://www.pivotaltracker.com/story/show/163704556)

---
+++ ci/tasks/setup-env.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -exu
+
+main() {
+  source bosh-src/ci/tasks/utils.sh
+
+  local build_dir="${PWD}"
+  local bbl_state_env_repo_dir=$PWD/bbl-state
+  local output_dir="$PWD/updated-bbl-state/"
+  local env_assets="$PWD/bosh-src/ci/acceptance"
+  BBL_STATE_DIR=bosh-stemcell-acceptance-env
+  export BBL_STATE_DIR
+
+  trap "commit_bbl_state_dir ${bbl_state_env_repo_dir} ${BBL_STATE_DIR} ${output_dir} 'Update bosh-stemcell-acceptance-env environment'" EXIT
+
+  mkdir -p "bbl-state/${BBL_STATE_DIR}"
+
+  pushd "bbl-state/${BBL_STATE_DIR}"
+    bbl version
+    bbl plan > bbl_plan.txt
+
+    # Customize environment
+    cp $env_assets/*.sh .
+
+    rm -rf bosh-deployment
+    ln -s ${build_dir}/bosh-deployment bosh-deployment
+
+    bbl --debug up
+
+    set +x
+    eval "$(bbl print-env)"
+    set -x
+    bosh upload-stemcell ${build_dir}/stemcell/*.tgz -n
+    bosh -d zookeeper deploy --recreate ${build_dir}/zookeeper-release/manifests/zookeeper.yml -n -o \
+			<(echo '{
+					"type": "replace",
+					"path": "/stemcells/alias=default/os",
+					"value": "ubuntu-xenial"
+				}'
+			)
+  popd
+}
+
+main "$@"
