e9ea96197a2abcc5188173e51b7a64d8a4b9ae34
xuliya <xuliya@xuliyas-mbp.cn.ibm.com>
2019-03-20 10:30:24 +0800

    Add a new job in building stemcell pipeline to OS reload an existing VM with the newly created SL community stemcell

---
+++ ci/softlayer/tasks/build-stemcell.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e -x -u
+
+function check_param() {
+  local name=$1
+  local value=$(eval echo '$'$name)
+  if [ "$value" == 'replace-me' ]; then
+    echo -e "\n[ERROR] environment variable $name must be set"
+    exit 1
+  fi
+}
+
+check_param PUBLISHED_BUCKET_NAME
+check_param OS_NAME
+check_param OS_VERSION
+
+export TASK_DIR=$PWD
+export VERSION=$( cat version/number | sed 's/\.0$//;s/\.0$//' )
+
+echo "[INFO] Install dependencies packages"
+apt-get update
+apt-get install curl -y
+
+tar zxvf stemcell/*.tgz  -C ./
+virtual-disk-image-uuid=`grep "virtual-disk-image-uuid:" stemcell.MF| cut -d ":" -f2`
+
+curl -X POST -d '{ \
+  "parameters":[ \
+         "FORCE", \
+         { \
+             "imageTemplateId": "$virtual-disk-image-uuid" \
+         } \
+  ] \
+ }' https://$SL_USERNAME:$SL_API_KEY@api.softlayer.com/rest/v3/SoftLayer_Virtual_Guest/74649319/reloadOperatingSystem.json
+
