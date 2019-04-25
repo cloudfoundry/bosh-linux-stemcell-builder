73f4ca30d25882273798dc87661836e0c626b53d
Joshua Aresty <joshua.aresty@emc.com>
2019-04-25 16:56:22 -0700

    Always add PATH to environment
    This allows for compatibility with CentOS because it does not contain a
    PATH entry in its /etc/environment file.
    
    [#165626736]
    
    Co-authored-by: Chris Tarazi <christarazi@pivotal.io>

--- stemcell_builder/stages/bosh_environment/apply.sh
+++ stemcell_builder/stages/bosh_environment/apply.sh
@@ -6,4 +6,4 @@ base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 source $base_dir/lib/prelude_bosh.bash
 
-sed -i '/PATH/s/"$/:\/var\/vcap\/bosh\/bin"/' "$chroot/etc/environment"
+sed -i '/^PATH/d;$a PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/var/vcap/bosh/bin"' "$chroot/etc/environment"
