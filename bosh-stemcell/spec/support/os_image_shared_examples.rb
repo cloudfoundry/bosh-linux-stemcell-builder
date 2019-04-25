fdfda3332b49e489472c629eb89e3a4db3026bf7
Joshua Aresty <joshua.aresty@emc.com>
2019-04-25 10:33:36 -0700

    Make apply.sh executable and check content
    [#164818162]

--- bosh-stemcell/spec/support/os_image_shared_examples.rb
+++ bosh-stemcell/spec/support/os_image_shared_examples.rb
@@ -4,7 +4,7 @@ shared_examples_for 'every OS image' do
 
   context 'etc_environment' do
     it 'should have /var/vcap/bosh/bin on the PATH' do
-      expect(etc_environment).to match(
+      expect(etc_environment.content).to match(
         %r{PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/var/vcap/bosh/bin"},
       )
     end
