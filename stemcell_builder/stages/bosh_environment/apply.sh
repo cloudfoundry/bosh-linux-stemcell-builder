908f8b361b10e6470896dec684a49be006c5bdad
Joshua Aresty <joshua.aresty@emc.com>
2019-04-26 09:42:15 -0700

    Use echo to append PATH
    Since `sed` is a stream editor, it is unable to append if the file is
    empty after deleting the existing `PATH` reference.
    
    [#165626736]

--- stemcell_builder/stages/bosh_environment/apply.sh
+++ stemcell_builder/stages/bosh_environment/apply.sh
@@ -7,4 +7,5 @@ source $base_dir/lib/prelude_apply.bash
 source $base_dir/lib/prelude_bosh.bash
 
 touch "$chroot/etc/environment"
-sed -i '/^PATH/d;$a PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/var/vcap/bosh/bin"' "$chroot/etc/environment"
+sed -i '/^PATH/d' "$chroot/etc/environment"
+echo 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/var/vcap/bosh/bin"' >> "$chroot/etc/environment"
