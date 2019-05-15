c0f90a8f4f9f68f30e1fcf50823b6ad0664ae210
Chris Tarazi <ctarazi@pivotal.io>
2019-05-15 15:32:21 -0700

    Remove unnecessary kernel module
    This kernel module is no longer required in kernels 4.8 and beyond. It
    was previously required for trusty, but we have deprecated that.

--- stemcell_builder/stages/system_google_modules/apply.sh
+++ stemcell_builder/stages/system_google_modules/apply.sh
@@ -5,8 +5,4 @@ set -e
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
-if [[ "${DISTRIB_CODENAME}" != 'xenial' ]]; then
-  echo "acpiphp" >> $chroot/etc/modules
-fi
-
 echo "virtio_scsi" >> $chroot/etc/modules
