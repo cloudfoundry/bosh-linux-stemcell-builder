5952a061dbc23cdc031d6a61b6c22a8e198c4e1b
Joshua Aresty <joshua.aresty@emc.com>
2019-05-16 11:15:50 -0700

    Require tmpdir in specs

--- bosh-stemcell/spec/spec_helper.rb
+++ bosh-stemcell/spec/spec_helper.rb
@@ -2,5 +2,6 @@ require 'rspec'
 require 'rspec/its'
 require 'fakefs/spec_helpers'
 require 'support/shellout_type_assertions.rb'
+require 'tmpdir'
 
 Dir.glob(File.expand_path('../support/**/*.rb', __FILE__)).each { |f| require(f) }
