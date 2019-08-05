77782093bce3a0edcc77df5cca21a480d97d25b9
Aakash Shah <ashah@pivotal.io>
2019-08-05 15:16:30 -0700

    Fix human typo: redundant "google-cloud-sdk" package
    Co-authored-by: Joshua Aresty <joshua.aresty@emc.com>

--- stemcell_builder/stages/system_google_packages/apply.sh
+++ stemcell_builder/stages/system_google_packages/apply.sh
@@ -18,7 +18,7 @@ declare set_hostname_path
 
 os_type="$(get_os_type)"
 if [[ "${os_type}" == "ubuntu" ]] ; then
-  pkg_mgr install "gce-compute-image-packages google-cloud-sdk google-compute-engine-oslogin python-google-compute-engine python3-google-compute-engine"
+  pkg_mgr install "gce-compute-image-packages google-compute-engine-oslogin python-google-compute-engine python3-google-compute-engine"
 
 
   # Hack: replace google metadata hostname with ip address (bosh agent might set a dns that it's unable to resolve the hostname)
