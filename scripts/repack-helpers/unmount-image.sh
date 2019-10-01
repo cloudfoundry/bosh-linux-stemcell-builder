a2a3579351434a6cfbd53103fd8ac3b8da261971
Aakash Shah <ashah@pivotal.io>
2019-10-01 11:57:45 -0700

    More sudo usages in repack-helpers
    Co-authored-by: Joshua Aresty <joshua.aresty@emc.com>

--- scripts/repack-helpers/unmount-image.sh
+++ scripts/repack-helpers/unmount-image.sh
@@ -9,6 +9,6 @@ fi
 mounted_image_directory=$(cat)
 mounted_loopback=$(mount | grep $mounted_image_directory | cut -f1 -d' ' | sed 's/p[[:digit:]]*$//')
 raw_disk=$(losetup -l | awk "\$1 == \"$mounted_loopback\" { print \$6 }")
-sudo umount $mounted_image_directory
-sudo kpartx -d $raw_disk
+umount $mounted_image_directory
+kpartx -d $raw_disk
 echo $raw_disk
