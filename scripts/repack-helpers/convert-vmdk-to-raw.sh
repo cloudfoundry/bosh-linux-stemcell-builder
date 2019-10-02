273815b67454e8b626280d3b62313e1c456e2b57
Joshua Aresty <joshua.aresty@emc.com>
2019-10-02 13:55:52 -0700

    Fixup variable name
    Co-authored-by: Chris Tarazi <ctarazi@pivotal.io>

--- scripts/repack-helpers/convert-vmdk-to-raw.sh
+++ scripts/repack-helpers/convert-vmdk-to-raw.sh
@@ -5,10 +5,11 @@ if [ -t 0 ]; then
   exit 2
 fi
 
+# This is intentional -- it is designed to be called in a pipe
 extracted_image_path=$(cat)
 
 converted_raw_path=$(mktemp -d)
 
-qemu-img convert $extracted_image/image-disk1.vmdk $converted_raw_path/disk.raw
+qemu-img convert $extracted_image_path/image-disk1.vmdk $converted_raw_path/disk.raw
 
 echo $converted_raw_path
