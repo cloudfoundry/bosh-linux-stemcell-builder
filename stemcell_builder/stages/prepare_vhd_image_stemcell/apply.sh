dc2fc236e923b46451b936375ad053b5f9749148
Aakash Shah <ashah@pivotal.io>
2019-10-04 15:21:49 -0700

    Fix azure root disk being non-integer
    * Certain versions of qemu don't match the final VHD file's size
    with the input .raw file [1]
    * This is an issue on Azure as the root disk must be a whole number in
    the units of MB [2]
    * The "force_size" flag fixes this
    
    [1] https://bugs.launchpad.net/qemu/+bug/1490611
    [2] https://docs.microsoft.com/en-us/azure/virtual-machines/linux/create-upload-generic?toc=%2Fazure%2Fvirtual-machines%2Flinux%2Ftoc.json#resizing-vhds
      "VHD images on Azure must have a virtual size aligned to 1 MB.
      Typically, VHDs created using Hyper-V are aligned correctly. If the
      VHD isn't aligned correctly, you may receive an error message similar
      to the following when you try to create an image from your VHD."
    
    [#168964419]

--- stemcell_builder/stages/prepare_vhd_image_stemcell/apply.sh
+++ stemcell_builder/stages/prepare_vhd_image_stemcell/apply.sh
@@ -8,7 +8,7 @@ source $base_dir/lib/prelude_apply.bash
 rm -f $work/root.vhd
 
 # The size of the VHD for Azure must be a whole number in megabytes.
-qemu-img convert -O vpc -o subformat=fixed $work/${stemcell_image_name} $work/root.vhd
+qemu-img convert -O vpc -o subformat=fixed,force_size $work/${stemcell_image_name} $work/root.vhd
 
 pushd $work
 tar zcf stemcell/image root.vhd
