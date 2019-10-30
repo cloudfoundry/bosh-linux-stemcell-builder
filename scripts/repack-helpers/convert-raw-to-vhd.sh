8f13f137229ee380be66804759953074c25e1a61
Joshua Aresty <joshua.aresty@emc.com>
2019-10-30 16:28:50 -0700

    Round to a whole number when converting to vhd
    - Azure expects round numbers of megabytes, so this is necessary.

--- scripts/repack-helpers/convert-raw-to-vhd.sh
+++ scripts/repack-helpers/convert-raw-to-vhd.sh
@@ -10,6 +10,13 @@ extracted_image_path=$(cat)
 
 converted_raw_path=$(mktemp -d)
 
+MB=$((1024*1024))
+size=$(qemu-img info -f raw --output json "$extracted_image_path" | \
+       awk 'match($0, /"virtual-size": ([0-9]+),/, val) {print val[1]}')
+
+rounded_size=$((($size/$MB + 1)*$MB))
+qemu-img resize $extracted_image_path $rounded_size
+
 # The size of the VHD for Azure must be a whole number in megabytes.
 qemu-img convert -O vpc -o subformat=fixed,force_size $extracted_image_path $converted_raw_path/root.vhd
 
