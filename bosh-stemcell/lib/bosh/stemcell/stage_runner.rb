7144684490e7104a8fb74f2864a3188abfeba683
Aakash Shah <ashah@pivotal.io>
2019-11-25 16:36:34 -0800

    Display stage durations
    Co-authored-by: Christopher Brown <cbrown@pivotal.io>

--- bosh-stemcell/lib/bosh/stemcell/stage_runner.rb
+++ bosh-stemcell/lib/bosh/stemcell/stage_runner.rb
@@ -1,6 +1,35 @@
 require 'bosh/core/shell'
 
 module Bosh::Stemcell
+  class BuildReport
+    def initialize
+      @stats = []
+    end
+
+    def measure_configure(name, &block)
+      start = Time.now.utc
+
+      puts "== Started configuring #{name} at #{start.strftime('%a %b %e %H:%M:%S %Z %Y')} =="
+      yield
+      @stats << {type: "configure", name: name, duration: Time.now.utc - start}
+    end
+
+    def measure_apply(name, &block)
+      start = Time.now.utc
+
+      puts "== Started applying #{name} at #{start.strftime('%a %b %e %H:%M:%S %Z %Y')} =="
+      yield
+      @stats << {type: "apply", name: name, duration: Time.now.utc - start}
+    end
+
+    def print_report
+      puts "=== Stage Duration Report"
+      @stats.each do |stage|
+        puts "[#{stage.fetch(:type)}/#{stage.fetch(:name)}] - #{"%.10f" % stage.fetch(:duration)}s"
+      end
+    end
+  end
+
   class StageRunner
 
     REQUIRED_UID=1000
@@ -10,22 +39,25 @@ module Bosh::Stemcell
       @command_env = options.fetch(:command_env)
       @settings_file = options.fetch(:settings_file)
       @work_path = options.fetch(:work_path)
+      @report = BuildReport.new
     end
 
     def configure_and_apply(stages, resume_from_stage = nil)
       stages = resume_from(stages, resume_from_stage)
       configure(stages)
       apply(stages)
+
+      @report.print_report
     end
 
     def configure(stages)
       stages.each do |stage|
         stage_config_script = File.join(build_path, 'stages', stage.to_s, 'config.sh')
 
-        puts "=== Configuring '#{stage}' stage ==="
-        puts "== Started #{Time.now.strftime('%a %b %e %H:%M:%S %Z %Y')} =="
-        if File.exists?(stage_config_script) && File.executable?(stage_config_script)
-          run_sudo_with_command_env("#{stage_config_script} #{settings_file}")
+        @report.measure_configure(stage) do
+          if File.exists?(stage_config_script) && File.executable?(stage_config_script)
+            run_sudo_with_command_env("#{stage_config_script} #{settings_file}")
+          end
         end
       end
     end
@@ -34,19 +66,16 @@ module Bosh::Stemcell
       stages.each do |stage|
         FileUtils.mkdir_p(work_path)
 
-        puts "=== Applying '#{stage}' stage ==="
-        puts "== Started #{Time.now.strftime('%a %b %e %H:%M:%S %Z %Y')} =="
+        @report.measure_apply(stage) do
+          begin
+            stage_apply_script = File.join(build_path, 'stages', stage.to_s, 'apply.sh')
 
-        begin
-          stage_apply_script = File.join(build_path, 'stages', stage.to_s, 'apply.sh')
-
-          run_sudo_with_command_env("#{stage_apply_script} #{work_path}")
-
-        rescue => _
-          puts "=== You can resume_from the '#{stage}' stage by using resume_from=#{stage} ==="
-          raise
+            run_sudo_with_command_env("#{stage_apply_script} #{work_path}")
+          rescue => _
+            puts "=== You can resume_from the '#{stage}' stage by using resume_from=#{stage} ==="
+            raise
+          end
         end
-
       end
     end
 
