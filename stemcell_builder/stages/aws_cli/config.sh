1a2dff465ca42550e46a74d8941238913d95f6b2
Shaan Sapra <shsapra@pivotal.io>
2020-02-20 17:06:17 -0800

    Backport: Bump s3cli to v0.0.95 [#174268356]
    [#171374620](https://www.pivotaltracker.com/story/show/171374620)

--- stemcell_builder/stages/aws_cli/config.sh
+++ stemcell_builder/stages/aws_cli/config.sh
@@ -9,6 +9,6 @@ source $base_dir/lib/prelude_config.bash
 cd $assets_dir
 rm -rf s3cli
 mkdir s3cli
-current_version=0.0.80
+current_version=0.0.95
 curl_five_times "s3cli/s3cli" "https://s3.amazonaws.com/s3cli-artifacts/s3cli-${current_version}-linux-amd64"
-echo "811dfe3f0dfc6b51d12aa3910069c6c90a15354e9823243a75c5f3762b4a9e3a s3cli/s3cli" | sha256sum -c -
+echo "4f280cadec594f8767d7d1398e76c06dadc0f566b5b17ec8f08062629f471221 s3cli/s3cli" | sha256sum -c -
