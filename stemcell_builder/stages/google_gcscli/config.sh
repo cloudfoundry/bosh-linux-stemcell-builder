798fcad22aa8075452c439f76631cb5f7dc3be87
Shaan Sapra <shsapra@pivotal.io>
2020-03-02 15:03:59 -0800

    Bump bosh-gcscli to 0.0.18
    [#171439750](https://www.pivotaltracker.com/story/show/171439750)

--- stemcell_builder/stages/google_gcscli/config.sh
+++ stemcell_builder/stages/google_gcscli/config.sh
@@ -9,7 +9,7 @@ source $base_dir/lib/prelude_config.bash
 cd $assets_dir
 rm -rf gcscli
 mkdir gcscli
-current_version=0.0.16
+current_version=0.0.18
 
 curl -L -o gcscli/gcscli https://s3.amazonaws.com/bosh-gcscli/bosh-gcscli-${current_version}-linux-amd64
-echo "94685c1a460203575df6c039cfdd841cc26fbec2 gcscli/gcscli" | sha1sum -c -
+echo "cfd7d76bdaea3027ea5687b971c2cbfeca7d4dd5 gcscli/gcscli" | sha1sum -c -
