2f351575c78f717781f873b37a6a1e242b4feb76
Diego Lemos <diego@diegolemos.net>
2020-07-22 15:11:00 +0100

    Make build.sh executable from localhost

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+REPO_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+
 function build() {
   echo $PWD
 
-  /opt/bosh/ci/docker/run os-image-stemcell-builder
-  whoami | grep ubuntu
-  if [[ "$?" -eq "0" ]]; then
-    echo "You're now ready to continue from the Build Steps section."
-  else
-    echo "Couldn't find user 'ubuntu'."
-    exit 1
-  fi
+  pushd "$REPO_DIR/ci/docker"
+    ./run os-image-stemcell-builder
+    whoami | grep ubuntu
 
-  bundle install --local
+    if [[ "$?" -eq "0" ]]; then
+      echo "You're now ready to continue from the Build Steps section."
+    else
+      echo "Couldn't find user 'ubuntu'."
+      exit 1
+    fi
 
-  # TODO(cdutra): build os image
+    bundle install --local
+
+    # TODO(cdutra): build os image
+  popd
 }
 
 build
