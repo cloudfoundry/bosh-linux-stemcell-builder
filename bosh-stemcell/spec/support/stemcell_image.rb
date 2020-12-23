8a1ae93212199076716ed519fabbe95835d93311
gubin <gubin@cn.ibm.com>
2020-12-23 13:33:20 +0800

    update stemcell_image.rb to align with two partitions in the Softlayer Bionic stemcell image

--- bosh-stemcell/spec/support/stemcell_image.rb
+++ bosh-stemcell/spec/support/stemcell_image.rb
@@ -4,13 +4,36 @@ require_relative 'shellout_type_assertions'
 
 RSpec.configure do |config|
   if ENV['STEMCELL_IMAGE']
-    disk_image = Bosh::Stemcell::DiskImage.new(image_file_path: ENV['STEMCELL_IMAGE'])
-    config.before(:suite) do |example|
-      disk_image.mount
-      ShelloutTypes::Chroot.chroot_dir = disk_image.image_mount_point
-    end
-    config.after(:suite) do |example|
-      disk_image.unmount
+    if config.filter[:exclude_on_softlayer]
+      disk_image = Bosh::Stemcell::DiskImage.new(image_file_path: ENV['STEMCELL_IMAGE'])
+      config.before(:suite) do |example|
+        disk_image.mount
+        ShelloutTypes::Chroot.chroot_dir = disk_image.image_mount_point
+      end
+      config.after(:suite) do |example|
+        disk_image.unmount
+      end
+    else
+      shell = Bosh::Core::Shell.new
+      verbose = true
+      image_file_path = ENV['STEMCELL_IMAGE']
+      device = shell.run("sudo losetup --show --find #{image_file_path}", output_command: verbose)
+      kpartx_output = shell.run("sudo kpartx -sav #{device}", output_command: verbose)
+      device_partition1 = kpartx_output.lines.first.split(' ')[2]
+      device_partition2 = kpartx_output.lines.last.split(' ')[2]
+      loopback_dev1 = "/dev/mapper/#{device_partition1}"
+      loopback_dev2 = "/dev/mapper/#{device_partition2}"
+      image_mount_point = Dir.mktmpdir
+      config.before(:suite) do |example|
+        shell.run("sudo mkdir #{image_mount_point}/boot", output_command: verbose)
+        shell.run("sudo mount #{loopback_dev2} #{image_mount_point}", output_command: verbose)
+        shell.run("sudo mount #{loopback_dev1} #{image_mount_point}/boot", output_command: verbose)
+        ShelloutTypes::Chroot.chroot_dir = image_mount_point
+      end
+      config.after(:suite) do |example|
+        shell.run("sudo umount #{image_mount_point}/boot", output_command: verbose)
+        shell.run("sudo umount #{image_mount_point}", output_command: verbose)
+      end
     end
   else
     warning = 'All stemcell_image tests are being skipped. STEMCELL_IMAGE needs to be set'
