e3cc540e4dede022b66b89e8cd99fc08378d3918
Maria Shaldybin <mariash@vmware.com>
2021-02-18 14:37:06 -0800

    Remove adding acpiphp module
    It was supposed to be only loaded in Trusty stemcell. The original
    commit that introduced it is 741f485675f13ec3cda19d375b8f30b1aa1c584c
    
    Co-authored-by: Maria Shaldybin <mariash@vmware.com>
    Co-authored-by: Kenneth Lakin <klakin@vmware.com>

--- stemcell_builder/stages/system_aws_modules/apply.sh
+++ stemcell_builder/stages/system_aws_modules/apply.sh
@@ -4,7 +4,3 @@ set -e
 
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
-
-if [[ "${DISTRIB_CODENAME}" != 'xenial' ]]; then
-  echo "acpiphp" >> $chroot/etc/modules
-fi
