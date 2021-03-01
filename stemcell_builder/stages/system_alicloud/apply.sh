feb384e291bb3be19576aa47b8d3d6e374be4bed
Stefan Wutz <stefan.wutz@wildbox-it.de>
2021-03-01 17:08:30 +0100

    Remove adding acpiphp module from alicloud

--- stemcell_builder/stages/system_alicloud/apply.sh
+++ stemcell_builder/stages/system_alicloud/apply.sh
@@ -5,10 +5,6 @@ set -e
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
-if [[ "${DISTRIB_CODENAME}" != 'xenial' ]]; then
-  echo "acpiphp" >> $chroot/etc/modules
-fi
-
 # Alicloud does not support ext4 feature 'metadata_csum'.
 # Remove it from root image and also disable it inside image for all ext4 filesystems created afterwards.
 sed -i "s/metadata_csum,//g" /etc/mke2fs.conf
