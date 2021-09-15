f5fdeab90a9680661f6bc0944e5082347cdd7593
Ops Manager <cf-tempest-eng@pivotal.io>
2021-09-15 10:19:14 -0400

    Removing umac-128-etm and umac-128
    [#178891940] Remove less secure ciphers from /etc/ssh/sshd_config in xenial stemcells

--- stemcell_builder/stages/base_ssh/apply.sh
+++ stemcell_builder/stages/base_ssh/apply.sh
@@ -76,7 +76,7 @@ echo 'Ciphers aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ct
 
 # Disallow Weak MACs
 sed "/^ *MACs/d" -i $chroot/etc/ssh/sshd_config
-echo 'MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com' >> $chroot/etc/ssh/sshd_config
+echo 'MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512,hmac-sha2-256' >> $chroot/etc/ssh/sshd_config
 
 cat << EOF > $chroot/etc/issue
 Unauthorized use is strictly prohibited. All access and activity
