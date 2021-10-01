8a82cc210fdf755b0fb7a3f211c43398f00bf4fe
ramonskie <ramonmakkelie@gmail.com>
2021-10-01 12:59:17 +0200

    bump ruby and remove the need for chruby

--- ci/docker/os-image-stemcell-builder-bionic/install-ruby.sh
+++ ci/docker/os-image-stemcell-builder-bionic/install-ruby.sh
@@ -3,19 +3,8 @@
 set -e
 set -x
 
-CHRUBY_VER="0.3.9"
-CHRUBY_URL=https://github.com/postmodern/chruby/archive/v${CHRUBY_VER}.tar.gz
-RUBY_INSTALL_VER="0.8.1"
+RUBY_INSTALL_VER="0.8.3"
 RUBY_INSTALL_URL=https://github.com/postmodern/ruby-install/archive/v${RUBY_INSTALL_VER}.tar.gz
-
-echo "Installing chruby v${CHRUBY_VER}..."
-wget -O chruby-${CHRUBY_VER}.tar.gz https://github.com/postmodern/chruby/archive/v${CHRUBY_VER}.tar.gz
-tar -xzvf chruby-${CHRUBY_VER}.tar.gz
-cd chruby-${CHRUBY_VER}/
-scripts/setup.sh
-cd ..
-rm -rf chruby-${CHRUBY_VER}/
-rm chruby-${CHRUBY_VER}.tar.gz
 
 echo "Installing ruby-install v${RUBY_INSTALL_VER}..."
 wget -O ruby-install-${RUBY_INSTALL_VER}.tar.gz $RUBY_INSTALL_URL
@@ -31,13 +20,11 @@ install_ruby() {
     local sha=$2
 
     echo "Installing ruby $version..."
-    ruby-install --cleanup --sha256 "$sha" ruby "$version"
+    ruby-install --jobs=2 --cleanup --system --sha256 "$sha" ruby "$version" -- --disable-install-rdoc
 
-    source /etc/profile.d/chruby.sh
-
-    chruby "ruby-$version"
     ruby -v
     gem update --system
+
 }
 
-install_ruby 3.0.1 d06bccd382d03724b69f674bc46cd6957ba08ed07522694ce44b9e8ffc9c48e2
+install_ruby 3.0.2 570e7773100f625599575f363831166d91d49a1ab97d3ab6495af44774155c40
