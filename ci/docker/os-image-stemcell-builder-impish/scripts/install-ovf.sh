ed83ce6cd6bcef69dfaf08fbd1cccd07470caab8
ramonskie <ramonmakkelie@gmail.com>
2021-11-12 16:55:28 +0100

    fix eula for ovftool

--- ci/docker/os-image-stemcell-builder-impish/scripts/install-ovf.sh
+++ ci/docker/os-image-stemcell-builder-impish/scripts/install-ovf.sh
@@ -6,5 +6,5 @@ cd /tmp
 echo "${OVF_TOOL_INSTALLER_SHA1} /tmp/ovftool_installer.bundle" | sha1sum -c -
 chmod a+x ./ovftool_installer.bundle
 ln -s /bin/cat /usr/local/bin/more
-echo -e "\nyes\n\n" | bash  ./ovftool_installer.bundle
+bash  ./ovftool_installer.bundle --eulas-agreed
 rm -rf ./ovftool_installer.bundle /tmp/vmware-root/ /usr/local/bin/more
