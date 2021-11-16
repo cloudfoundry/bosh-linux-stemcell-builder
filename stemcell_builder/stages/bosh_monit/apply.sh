df452cec7e1ec603e556e2fd1b0e93585c886782
Long Nguyen <nguyenlo@vmware.com>
2021-11-16 15:43:14 -0500

    Revert back to older monit (5.2.5)
    Signed-off-by: Brian Cunnie <bcunnie@vmware.com>

--- stemcell_builder/stages/bosh_monit/apply.sh
+++ stemcell_builder/stages/bosh_monit/apply.sh
@@ -6,12 +6,11 @@ base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 source $base_dir/lib/prelude_bosh.bash
 
-monit_basename=monit-5.29.0
+monit_basename=monit-5.2.5
 monit_archive=$monit_basename.tar.gz
 
 mkdir -p $chroot/$bosh_dir/src
 cp -r $dir/assets/$monit_archive $chroot/$bosh_dir/src
-
 
 pkg_mgr install "zlib1g-dev"
 
@@ -19,7 +18,7 @@ run_in_bosh_chroot $chroot "
 cd src
 tar zxvf $monit_archive
 cd $monit_basename
-./configure --prefix=$bosh_dir --without-ssl --without-pam
+./configure --prefix=$bosh_dir --without-ssl CFLAGS="-fcommon"
 make -j4 && make install
 "
 
