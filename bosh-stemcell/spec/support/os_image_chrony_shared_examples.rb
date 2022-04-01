5a726b9ecccae8dc02d0f2d84d388f31b4a0c7d7
Justin Webster <justin.webster@gmail.com>
2022-04-01 15:40:27 -0700

    bosh_ntp stage: Update the 'bosh/bin/sync-time' file for RHEL 8 compatibility. NOTE: This is related to the `describe 'chrony service'` spec (which was already separately updated for RHEL 8 compatibility), but neither that spec nor the previous specs for the 'bosh/bin/sync-time' file were failing prior to this change (since the stage and specs were both consistent with each other).

--- bosh-stemcell/spec/support/os_image_chrony_shared_examples.rb
+++ bosh-stemcell/spec/support/os_image_chrony_shared_examples.rb
@@ -1,9 +1,20 @@
 shared_examples_for 'an os with chrony' do
   describe '(stig: V-38620 V-38621)' do
     describe file('/var/vcap/bosh/bin/sync-time') do
+      let(:chrony_service_name) do
+        if (ENV['OS_NAME'] == 'rhel' && ENV['OS_VERSION'] == '8')
+          # NOTE: The service is named 'chronyd' on RHEL 8.
+          # SEE: https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html/configuring_basic_system_settings/using-chrony-to-configure-ntp_configuring-basic-system-settings
+          # SEE: https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html/configuring_basic_system_settings/using-chrony_configuring-basic-system-settings
+          'chronyd'
+        else
+          'chrony'
+        end
+      end
+
       it { should be_file }
-      its(:content) { should match(/systemctl restart chrony\.service/) }
-      its(:content) { should match(/chronyc waitsync 10/) }
+      it('restarts the chrony service') { expect(subject.content).to match(/^systemctl restart #{chrony_service_name}\.service$/) }
+      its(:content) { should match(/^chronyc waitsync 10$/) }
     end
 
     describe 'chrony.conf file' do
