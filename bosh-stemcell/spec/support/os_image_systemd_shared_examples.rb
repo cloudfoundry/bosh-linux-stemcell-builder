499c396df2285e88c8ac3265df3780b1e71751be
Justin Webster <justin.webster@gmail.com>
2022-04-01 15:39:39 -0700

    Specs: rhel 8 compatibility: Fix `describe service('chrony')` spec failure on RHEL 8.
    Fixes/resolves the following spec failures:
    
    ```
      6) RHEL 8 OS image behaves like a systemd-based OS image systemd services chrony is expected to be enabled
         Failure/Error: it { should be_enabled }
           expected `#<ShelloutTypes::Service:0x0000558d1350d160 @service="chrony", @chroot=#<ShelloutTypes::Chroot:0x0000558d1350d188>>.enabled?` to be truthy, got false
         Shared Example Group: "a systemd-based OS image" called from ./spec/os_image/rhel_8_spec.rb:7
         # ./spec/support/os_image_systemd_shared_examples.rb:12:in `block (4 levels) in <top (required)>'
    ```

--- bosh-stemcell/spec/support/os_image_systemd_shared_examples.rb
+++ bosh-stemcell/spec/support/os_image_systemd_shared_examples.rb
@@ -8,7 +8,20 @@ shared_examples_for 'a systemd-based OS image' do
       it { should_not be_enabled }
     end
 
-    describe service('chrony') do
+    describe 'chrony service' do
+      let(:chrony_service_name) do
+        if (ENV['OS_NAME'] == 'rhel' && ENV['OS_VERSION'] == '8')
+          # NOTE: The service is named 'chronyd' on RHEL 8.
+          # SEE: https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html/configuring_basic_system_settings/using-chrony-to-configure-ntp_configuring-basic-system-settings
+          # SEE: https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html/configuring_basic_system_settings/using-chrony_configuring-basic-system-settings
+          'chronyd'
+        else
+          'chrony'
+        end
+      end
+
+      subject { service(chrony_service_name) }
+
       it { should be_enabled }
     end
 
