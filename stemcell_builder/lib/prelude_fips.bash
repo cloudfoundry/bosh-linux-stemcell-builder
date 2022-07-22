c77bce3e1d98a2d7dab66fafbb909e5a0a6089f4
Thomas Bechtold <thomas.bechtold@canonical.com>
2022-07-22 13:39:08 +0200

    Fix grub cmdline fips=1 setting
    Commit fb762e7d0b61b8 added the change wronly and the build failed
    with:
    
    prelude_fips.bash: line 61: GRUB_CMDLINE_LINUX_DEFAULT: \
        unbound variable
    
    This is now fixed.

--- stemcell_builder/lib/prelude_fips.bash
+++ stemcell_builder/lib/prelude_fips.bash
@@ -58,7 +58,9 @@ function install_and_hold_packages() {
 
 
 function write_fips_cmdline_conf() {
-    run_in_chroot ${chroot} "echo 'GRUB_CMDLINE_LINUX_DEFAULT=\"$GRUB_CMDLINE_LINUX_DEFAULT fips=1\"' > /etc/default/grub.d/99-fips.cfg"
+    cat << "EOF" >> "${chroot}/etc/default/grub.d/99-fips.cfg"
+GRUB_CMDLINE_LINUX_DEFAULT="$GRUB_CMDLINE_LINUX_DEFAULT fips=1"
+EOF
 }
 
 
