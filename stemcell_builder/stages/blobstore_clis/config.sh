81313c35476acbe88ab3de024efeca61e00d8d75
aram price <pricear@vmware.com>
2022-08-30 16:35:55 -0700

    A `config` stage should include `prelude_config.sh`
    ... not `prelude_apply.sh`
    
    Fixes:
    ```
    sudo env ESM_TOKEN='REDACTED' /mnt/stemcells/google/kvm/ubuntu/build/build/stages/blobstore_clis/config.sh /mnt/stemcells/google/kvm/ubuntu/build/build/etc/settings.bash 2>&1
    ++++ dirname /mnt/stemcells/google/kvm/ubuntu/build/build/stages/blobstore_clis/config.sh
    +++ dir=/mnt/stemcells/google/kvm/ubuntu/build/build/stages/blobstore_clis
    +++ assets_dir=/mnt/stemcells/google/kvm/ubuntu/build/build/stages/blobstore_clis/assets
    ++++ readlink -nf /mnt/stemcells/google/kvm/ubuntu/build/build/stages/blobstore_clis/../..
    +++ base_dir=/mnt/stemcells/google/kvm/ubuntu/build/build
    ++++ readlink -nf /mnt/stemcells/google/kvm/ubuntu/build/build/lib
    +++ lib_dir=/mnt/stemcells/google/kvm/ubuntu/build/build/lib
    ++++ readlink -nf /mnt/stemcells/google/kvm/ubuntu/build/build/skeleton
    +++ skeleton_dir=/mnt/stemcells/google/kvm/ubuntu/build/build/skeleton
    +++ settings_file=/mnt/stemcells/google/kvm/ubuntu/build/build/stages/blobstore_clis/assets/settings.bash
    ++ source /mnt/stemcells/google/kvm/ubuntu/build/build/lib/helpers.sh
    +++ declare -a on_exit_items
    +++ on_exit_items=()
    ++ work=/mnt/stemcells/google/kvm/ubuntu/build/build/etc/settings.bash
    ++ chroot=/mnt/stemcells/google/kvm/ubuntu/build/build/etc/settings.bash/chroot
    ++ mkdir -p /mnt/stemcells/google/kvm/ubuntu/build/build/etc/settings.bash /mnt/stemcells/google/kvm/ubuntu/build/build/etc/settings.bash/chroot
    mkdir: cannot create directory ‘/mnt/stemcells/google/kvm/ubuntu/build/build/etc/settings.bash’: File exists
    mkdir: cannot create directory ‘/mnt/stemcells/google/kvm/ubuntu/build/build/etc/settings.bash’: Not a directory
    
    ```
    
    Signed-off-by: Brian Cunnie <bcunnie@vmware.com>

--- stemcell_builder/stages/blobstore_clis/config.sh
+++ stemcell_builder/stages/blobstore_clis/config.sh
@@ -2,7 +2,7 @@
 set -e
 
 base_dir=$(readlink -nf "$(dirname "${0}")/../..")
-source "${base_dir}/lib/prelude_apply.bash"
+source "${base_dir}/lib/prelude_config.bash"
 
 # DAV CLI
 dav_cli_binary=bosh-blobstore-dav
