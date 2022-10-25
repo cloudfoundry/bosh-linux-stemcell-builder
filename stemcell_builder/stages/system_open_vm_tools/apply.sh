77f11b2dbdb8b11fd90005964f223448b14d988f
Kenneth Lakin <klakin@vmware.com>
2022-10-24 19:03:09 -0500

    Compiles open-vm-tools for Xenial
    Initial work to compile open-vm-tools from source, now that Canonical is
    not going to update that package for Xenial.
    
    Signed-off-by: Daniel Felipe Ochoa <danielfelipo@vmware.com>
    Signed-off-by: Kenneth Lakin <klakin@vmware.com>

--- stemcell_builder/stages/system_open_vm_tools/apply.sh
+++ stemcell_builder/stages/system_open_vm_tools/apply.sh
@@ -6,9 +6,38 @@ base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 source $base_dir/lib/prelude_bosh.bash
 
-# Installation on CentOS requires v7
-pkg_mgr install open-vm-tools
-
+function compile_package {
+  run_in_chroot $chroot "
+    cd /tmp
+    apt install -y autoconf \
+    libtool \
+    libmspack-dev \
+    libmspack0 \
+    libglib2.0-dev \
+    libglib2.0-0 \
+    libxmlsec1-dev \
+    libxmlsec1
+    wget https://github.com/vmware/open-vm-tools/releases/download/stable-12.1.0/open-vm-tools-12.1.0-20219665.tar.gz
+    tar xvf open-vm-tools-12.1.0-20219665.tar.gz
+    cd open-vm-tools-12.1.0-20219665
+    autoreconf -i
+    ./configure --without-pam --without-gtk2 --without-gtk3 --without-gtkmm3 --without-gtkmm --without-x
+    make
+    make install
+    ldconfig
+    apt remove -y autoconf \
+    libtool \
+    libmspack-dev \
+    libglib2.0-dev \
+    libxmlsec1-dev
+    "
+}
+if [[ "${DISTRIB_CODENAME}" == "xenial" ]]; then
+  compile_package
+else
+  # Installation on CentOS requires v7
+  pkg_mgr install open-vm-tools
+fi
 # open-vm-tools installs unwanted fusermount binary
 run_in_chroot $chroot "rm -f /usr/bin/fusermount"
 
