c853a4acd96f0c248b8c6ae1bdeb4e272b3c2f23
Long Nguyen <nguyenlo@vmware.com>
2023-04-17 11:39:04 -0400

    Moving phc conf to chrony config, phc is not picked as sources in chrony. phc is now correctly working as possible source with it being configured as a conf
    [#184816605] Fix chrony azure phc config and update Ops Manager documentation

--- stemcell_builder/stages/bosh_azure_chrony/apply.sh
+++ stemcell_builder/stages/bosh_azure_chrony/apply.sh
@@ -6,7 +6,7 @@ base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 source $base_dir/lib/prelude_bosh.bash
 
-cat > $chroot/etc/chrony/sources.d/azure_ptp.sources <<EOF
+cat > $chroot/etc/chrony/conf.d/azure_ptp.conf <<EOF
 # created by $0
 # https://docs.microsoft.com/en-us/azure/virtual-machines/linux/time-sync#chrony
 refclock PHC /dev/ptp0 poll 3 dpoll -2 offset 0
