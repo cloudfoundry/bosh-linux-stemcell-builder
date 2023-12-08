c182aace0a9cf9ecd39191d24ae1b1500d3f73f2
ramonskie <ramonmakkelie@gmail.com>
2023-12-08 11:31:17 +0100

    use snapshot apt when building from tag

--- stemcell_builder/stages/base_apt/apply.sh
+++ stemcell_builder/stages/base_apt/apply.sh
@@ -8,11 +8,21 @@ source $base_dir/lib/prelude_apply.bash
 mount --bind /sys "$chroot/sys"
 add_on_exit "umount $chroot/sys"
 
-cat > "$chroot/etc/apt/sources.list" <<EOS
-deb http://archive.ubuntu.com/ubuntu $DISTRIB_CODENAME main universe multiverse
-deb http://archive.ubuntu.com/ubuntu $DISTRIB_CODENAME-updates main universe multiverse
-deb http://security.ubuntu.com/ubuntu $DISTRIB_CODENAME-security main universe multiverse
+# check if current git branch is a tag and use snapshot date from the last commit message in that tag
+if git log -1 --format='%D' |grep "HEAD, tag:"; then
+  DATE=$(git log -1 --format="%at" | xargs -I{} date -d @{} +%Y%m%dT%H%M%SZ)
+  cat > "$chroot/etc/apt/sources.list" <<EOS
+  deb http://snapshot.ubuntu.com/ubuntu/$DATE $DISTRIB_CODENAME main universe multiverse
+  deb http://snapshot.ubuntu.com/ubuntu/$DATE $DISTRIB_CODENAME-updates main universe multiverse
+  deb http://snapshot.ubuntu.com/ubuntu/$DATE $DISTRIB_CODENAME-security main universe multiverse
 EOS
+else
+  cat > "$chroot/etc/apt/sources.list" <<EOS
+  deb http://archive.ubuntu.com/ubuntu $DISTRIB_CODENAME main universe multiverse
+  deb http://archive.ubuntu.com/ubuntu $DISTRIB_CODENAME-updates main universe multiverse
+  deb http://security.ubuntu.com/ubuntu $DISTRIB_CODENAME-security main universe multiverse
+EOS
+fi
 
 # Upgrade systemd/upstart first, to prevent it from messing up our stubs and starting daemons anyway
 pkg_mgr install systemd
