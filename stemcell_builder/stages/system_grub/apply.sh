7de2c0a1be2300fb9a4ef1938e3ee4fc5b5342c9
Long Nguyen <long.nguyen@broadcom.com>
2024-02-16 12:00:18 -0800

    Stage "system_grub": remove legacy grub, add EFI
    We remove GRUB version 1; Ubuntu switched its default from GRUB version
    1 (GRUB Legacy) to GRUB 2 in 2009. Leaving GRUB Legacy made the stage
    unnecessarily complicated.
    
    We also add EFI support to the GRUB 2 loader.
    
    Warning: may break non-EFI stemcells.
    
    Signed-off-by: Brian Cunnie <brian.cunnie@broadcom.com>

--- stemcell_builder/stages/system_grub/apply.sh
+++ stemcell_builder/stages/system_grub/apply.sh
@@ -5,34 +5,7 @@ set -e
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
-preferred=grub2
-fallback=grub
-
-if pkg_exists $preferred; then
-  pkg_mgr install $preferred
-elif pkg_exists $fallback; then
-  pkg_mgr install $fallback
-else
-  echo "Can't find grub or grub2 package to install"
-  exit 2
-fi
-
-if [ -d $chroot/usr/lib/grub/x86* ] # classic GRUB on Ubuntu
-then
-
-  rsync -a $chroot/usr/lib/grub/x86*/ $chroot/boot/grub/
-
-elif [ -d $chroot/usr/lib/grub/i386* ] # grub-pc on bionic
-then
-
-  rsync -a $chroot/usr/lib/grub/i386*/ $chroot/boot/grub/
-
-else
-
-  echo "Can't find GRUB or GRUB 2 files, exiting"
-  exit 2
-
-fi
+pkg_mgr install grub2 grub-efi-amd64-bin
 
 # When a kernel is installed, update-grub is run per /etc/kernel-img.conf.
 # It complains when /boot/grub/menu.lst doesn't exist, so create it.
