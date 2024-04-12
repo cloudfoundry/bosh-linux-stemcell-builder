adc01a5a9e1aaa0dec20d9385ba1127b7fed2ba1
ramonskie <ramonmakkelie@gmail.com>
2024-04-12 10:20:22 +0200

    bump static libs

--- stemcell_builder/stages/dev_tools_config/assets/generate_dev_tools_file_list.sh
+++ stemcell_builder/stages/dev_tools_config/assets/generate_dev_tools_file_list.sh
@@ -26,8 +26,16 @@ PACKAGES=( \
   cpp-9 \
   cpp-10 \
   cpp-11 \
+  cpp-12 \
+  cpp-13 \
   g++-5 \
   g++-7 \
+  g++-8 \
+  g++-9 \
+  g++-10 \
+  g++-11 \
+  g++-12 \
+  g++-13 \
   gcc-5 \
   gcc-6 \
   gcc-7 \
@@ -37,6 +45,7 @@ PACKAGES=( \
   gcc-11 \
   gcc-12 \
   gcc-13 \
+  gcc-14 \
   gcc-5-base \
   gcc-6-base \
   gcc-7-base \
@@ -46,19 +55,27 @@ PACKAGES=( \
   gcc-11-base \
   gcc-12-base \
   gcc-13-base \
+  gcc-14-base \
   clang \
-  clang-14 \
-  lib32gcc-s1 \
-  lib32stdc++6 \
+  clang-17 \
+  clang-18 \
+  libgcc-s1 \
+  libstdc++6 \
   libc6-i386 \
-  libclang-common-14-dev \
-  libclang-cpp14 \
-  libclang1-14 \
+  libclang-common-17-dev \
+  libclang-common-18-dev \
+  libclang-cpp17 \
+  libclang-cpp18 \
+  libclang1-17 \
+  libclang1-18 \
   libgc1 \
   libllvm14 \
   libobjc-11-dev \
+  libobjc-12-dev \
+  libobjc-13-dev \
   libobjc4 \
   llvm-14-linker-tools \
+  llvm-18-linker-tools \
 
 )
 
