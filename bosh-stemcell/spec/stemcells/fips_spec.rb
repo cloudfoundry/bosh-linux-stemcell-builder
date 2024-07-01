59120a364fb74962f9886c09368de072b597adbe
Aram Price <aram.price@broadcom.com>
2024-07-01 11:52:42 -0700

    Vary location of grub config if the stemcell is EFI or not
    Co-authored-by: Chris Selzo <chris.selzo@broadcom.com>

--- bosh-stemcell/spec/stemcells/fips_spec.rb
+++ bosh-stemcell/spec/stemcells/fips_spec.rb
@@ -36,7 +36,13 @@ describe 'FIPS Stemcell', os_image: true do
   end
 
   context 'installed by image_install_grub for fips kernel' do
-    describe file('/boot/efi/EFI/grub/grub.cfg') do
+    grub_file = if ENV['STEMCELL_INFRASTRUCTURE'] == 'vsphere'
+                  '/boot/efi/EFI/grub/grub.cfg'
+                else
+                  '/boot/grub/grub.cfg'
+                end
+
+    describe file(grub_file) do
       it { should be_file }
       its(:content) { should match %r{linux\t/boot/vmlinuz-\S+-fips root=UUID=\S* ro } }
       if use_iaas_kernel
