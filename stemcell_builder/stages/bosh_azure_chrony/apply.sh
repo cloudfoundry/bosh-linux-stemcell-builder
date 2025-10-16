33c82767c6eca799c90f70b4dc8f0341ac7bd20a
Adrian Joian <6505576+naioja@users.noreply.github.com>
2025-10-16 17:09:05 +0200

    Improve chrony reliability on Azure: add systemd override (FIFO scheduling, priority 50, OOMScoreAdjust -500) change Hyper-V PTP refclock poll 3 -> -1 (sampling interval max from 8s to 0.5s; with dpoll -2 dynamic range shifts from 2-8s to 0.125-0.5s) for tighter offset/jitter. (#442)

--- stemcell_builder/stages/bosh_azure_chrony/apply.sh
+++ stemcell_builder/stages/bosh_azure_chrony/apply.sh
@@ -6,8 +6,24 @@ base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 source $base_dir/lib/prelude_bosh.bash
 
+mkdir -p /etc/systemd/system/chrony.service.d
+
+cat > $chroot/etc/systemd/system/chrony.service.d/chrony-systemd-override.conf <<EOF
+# created by $0
+[Service]
+# Set the CPU scheduling policy to FIFO (First-In, First-Out), a real-time policy.
+CPUSchedulingPolicy=fifo
+
+# Set the real-time priority to the highest possible value (99).
+# This ensures chronyd runs before any other non-kernel, non-real-time tasks.
+CPUSchedulingPriority=50
+
+# Make the process less likely to be killed by the OOM killer
+OOMScoreAdjust=-500
+EOF
+
 cat > $chroot/etc/chrony/conf.d/azure_ptp.conf <<EOF
 # created by $0
 # https://docs.microsoft.com/en-us/azure/virtual-machines/linux/time-sync#chrony
-refclock PHC /dev/ptp_hyperv poll 3 dpoll -2 offset 0 stratum 2
+refclock PHC /dev/ptp_hyperv poll -1 dpoll -2 offset 0 stratum 2
 EOF
