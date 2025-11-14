31d7d7eced33575d96e4fd64aecadf93784b1110
Sebastian Heid <8442432+s4heid@users.noreply.github.com>
2025-11-14 13:44:16 +0100

    Consolidate Azure package installs

--- stemcell_builder/stages/system_azure_init/apply.sh
+++ stemcell_builder/stages/system_azure_init/apply.sh
@@ -5,7 +5,8 @@ set -e
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
-packages="python3 python3-pyasn1 python3-setuptools python3-distro python-is-python3 cloud-init"
+packages="python3 python3-pyasn1 python3-setuptools python3-distro python-is-python3 \
+cloud-init linux-cloud-tools-common linux-cloud-tools-generic"
 pkg_mgr install $packages
 
 wala_release=2.14.0.1
@@ -67,6 +68,5 @@ cat $chroot/etc/rsyslog.d/21-cloudinit.conf >> $chroot/etc/rsyslog.d/50-default.
 rm $chroot/etc/rsyslog.d/21-cloudinit.conf
 
 
-# Installing Hyper-V KVP daemon via linux-cloud-tools
-pkg_mgr install "linux-cloud-tools-common linux-cloud-tools-generic"
+# Enable Hyper-V KVP daemon (installed via linux-cloud-tools)
 run_in_chroot "$chroot" "systemctl enable hv-kvp-daemon.service"
