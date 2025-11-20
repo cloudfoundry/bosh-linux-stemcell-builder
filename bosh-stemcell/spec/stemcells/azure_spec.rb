6d75ce31584ebe027008e8ee9dba82ebaeb6fcd6
Sebastian Heid <8442432+s4heid@users.noreply.github.com>
2025-11-20 15:59:06 +0000

    Add Hyper-V KVP Daemon to Azure Stemcells (#453)
    * Add hvperv-kv daemon for Azure stemcells
    
    Azure runs on the Hyper-V hypervisor, and Linux VMs rely on Linux Integration
    Services (LIS) to communicate with the host. These services include kernel
    modules (like hv_utils) and daemons (hv_kvp_daemon, hv_vss_daemon, etc.) that
    handle:
    
    - Host-to-guest communication
    - IP address reporting
    - VM metadata exchange
    - Backup coordination (via VSS)
    
    The hv_kvp_daemon specifically manages key-value pair exchange between the
    Azure host and the Linux guest. If the daemon is missing, Azure may not
    be able to retrieve guest metadata (e.g., hostname, IP address) This can
    affect Azure Resource Graph queries, scripts that rely on guest-reported
    data, backup and monitoring tools that use KVP for coordination
    
    cloud-init writes warnings about events that could not be sent to hv-kvp:
    
    ```txt
    $ grep kvp /var/log/cloud-init.log
    failed to truncate kvp pool file, [Errno 2] No such file or directory: '/var/lib/hyperv/.kvp_pool_1'
    failed posting events to kvp, [Errno 2] No such file or directory: '/var/lib/hyperv/.kvp_pool_1'
    ```
    
    * Add missing tests for waagent.service
    
    * Consolidate Azure package installs

--- bosh-stemcell/spec/stemcells/azure_spec.rb
+++ bosh-stemcell/spec/stemcells/azure_spec.rb
@@ -79,4 +79,40 @@ describe 'Azure Stemcell', stemcell_image: true do
       its(:content) { should match /Unmanaged=yes/ }
     end
   end
+
+  context 'installed by system_azure_init', {
+    exclude_on_alicloud: true,
+    exclude_on_aws: true,
+    exclude_on_google: true,
+    exclude_on_vcloud: true,
+    exclude_on_vsphere: true,
+    exclude_on_warden: true,
+    exclude_on_openstack: true,
+    exclude_on_softlayer: true,
+  } do
+    describe 'Hyper-V KVP daemon' do
+      describe command('which hv_kvp_daemon') do
+        its(:exit_status) { should eq 0 }
+      end
+
+      describe service('hv-kvp-daemon') do
+        it { should be_enabled }
+      end
+    end
+
+    describe 'WALinuxAgent configuration' do
+      describe file('/etc/waagent.conf') do
+        it { should be_owned_by('root') }
+      end
+
+      describe file('/lib/systemd/system/walinuxagent.service') do
+        it { should be_mode(0644) }
+        it { should be_owned_by('root') }
+      end
+
+      describe service('walinuxagent') do
+        it { should be_enabled }
+      end
+    end
+  end
 end
