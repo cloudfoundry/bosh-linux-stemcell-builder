87f20c0a152a69b74dc672f7abbd1e3c8a79538e
Julian Hjortshoj <julian.hjortshoj@broadcom.com>
2025-12-09 16:21:49 -0800

    Add CloudInit step to vsphere agent settings
    This allows the agent to fetch settings from cloudinit in vsphere instead of mounting the CD-ROM. Note that this change shouln't be merged until there is a compatible agent binary available.

--- stemcell_builder/stages/bosh_vsphere_agent_settings/apply.sh
+++ stemcell_builder/stages/bosh_vsphere_agent_settings/apply.sh
@@ -16,6 +16,8 @@ cat > $chroot/var/vcap/bosh/agent.json <<JSON
     "Settings": {
       "Sources": [
         {
+          "Type": "CloudInit"
+        }, {
           "Type": "CDROM",
           "FileName": "env"
         }
