2b9c9af8e4f8cecf9bfa465291850eed01a2ae58
Michael Both <mboth@anynines.com>
2026-01-08 16:53:11 +0100

    Update stemcell-repack-debugger (#452)
    * Always remove all created temp dirs
    
    * Add variable to write patched stemcell to a different file
    
    * Add the invisible but obvious option to choose a different tmp dir
    
    * Make script compatible with both Jammy and Noble
    
    * Fix typo in script file name
    
    * Add manual version setting and autobump based on parameter or output tgz
    
    * Replace yq with bosh int
    
    * Replace yq with bosh int

--- scripts/stemcell-repack-debugger.sh
+++ scripts/stemcell-repack-debugger.sh
@@ -9,13 +9,17 @@ show_help() {
     echo "  --help, -h                Show this help message and exit"
     echo "  --debug                   Enable debug mode"
     echo "  --debug_pub_key FILE      Set a multiline public key file"
-    echo "  --bump-version            Bump the version in the stemcell with 0.0.<timestamp>"
+    echo "  --bump-version[=version]  Bump the version in the stemcell to a dev version that will be increased if you use the same stemcell tgz, e.g. <version>+dev.<int>"
+    echo "                            The optional version can be used instead of the stemcell version and increased if it is already a dev version"
+    echo "                            If als OUT is specified and the file is present and a tgz containing stemcell.MF, the version information is automatically used"
     echo
     echo "Environment Variables:"
     echo "  stemcell_tgz              Path to the stemcell tarball *REQUIRED*"
     echo "  AGENT_BINARY              Path to the agent binary to be copied into the stemcell *OPTIONAL*"
     echo "  AGENT_JSON                Path to the agent JSON configuration file *OPTIONAL*"
     echo "  BOSH_DEBUG_PUB_KEY        Public key for the BOSH debug user in a single line *OPTIONAL*"
+    echo "  OUT                       Path to the patched stemcell tarball *OPTIONAL*"
+    echo "  TMPDIR                    Path with enough disk space to hold all temporary files for repacking *OPTIONAL*"
     echo
     echo "Example:"
     echo "  export stemcell_tgz=\"/home/username/workspace/bosh/bosh-linux-stemcell-builder/tmp/bosh-stemcell-0.0.8-google-kvm-ubuntu-noble-go_agent.tgz\""
@@ -48,8 +52,13 @@ for arg in "$@"; do
             convert_cert_file="$2"
             shift 2
             ;;
-        --bump-version)
+        --bump-version*)
             bump_version=true
+            if [ -n "$OUT" -a -f "$OUT" ]; then
+                version=$(bosh int <(tar xf "$OUT" stemcell.MF --occurrence=1 -O) --path /version)
+            else
+                version=$(echo "$arg" | awk -F'=' '{print $2}')
+            fi
             shift
             ;;
         *)
@@ -73,23 +82,39 @@ temp_dir=$(mktemp -d)
 stemcell_dir=${temp_dir}/stemcell
 image_dir=${temp_dir}/image
 mkdir -p $stemcell_dir $image_dir
-trap 'rm -rf "${temp_dir}"' EXIT
+traps='rm -rf "${temp_dir}";'
+trap "$traps" EXIT
 
 # Repack stemcell
 cd $stemcell_dir
 tar xvf $stemcell_tgz
-new_ver=`date +%s`
 
 # Update stemcell with new agent
 cd $image_dir
 tar xvf $stemcell_dir/image
+DISK_NAME=disk.raw
+[ -f root.img ] && DISK_NAME=root.img
 mnt_dir=$(mktemp -d)
-trap 'rm -rf "${mnt_dir}"' EXIT
-device=$(sudo kpartx -sav disk.raw | grep '^add' | tail -n1 | cut -d' ' -f3)
+traps+='rm -rf "${mnt_dir}";'
+trap "${traps}" EXIT
+device=$(sudo kpartx -sav $DISK_NAME | grep '^add' | tail -n1 | cut -d' ' -f3)
 sudo mount -o loop,rw /dev/mapper/$device $mnt_dir
 
 if [ "$bump_version" = true ]; then
-    echo -n "0.0.${new_ver}" | sudo tee "$mnt_dir/var/vcap/bosh/etc/stemcell_version"
+    [ -z "$version" ] && version=$(bosh int $stemcell_dir/stemcell.MF --path /version)
+    base_version=$(echo -n "$version" | awk -F'+' '{print $1}')
+    dev_version=$(echo -n "$version" | awk -F'+' '{print $2}')
+    if [ -n "$dev_version" ]; then
+        dev_version=$(echo -n "$dev_version" | sed -e 's/dev[.]//')
+        dev_version=$((dev_version+1))
+    else
+        dev_version=1
+    fi
+    new_version="${base_version}+dev.${dev_version}"
+sudo cat "$mnt_dir/var/vcap/bosh/etc/stemcell_version"
+    echo -n "${new_version}" | sudo tee "$mnt_dir/var/vcap/bosh/etc/stemcell_version"
+sudo cat "$mnt_dir/var/vcap/bosh/etc/stemcell_version"
+    sed -r -i "s/^([[:space:]]*)version: .*/\1version: ${new_version}/" $stemcell_dir/stemcell.MF
 fi
 
 if [ -n "$AGENT_BINARY" ]; then
@@ -112,14 +137,12 @@ EOF
 fi
 
 sudo umount $mnt_dir
-sudo kpartx -dv disk.raw
+sudo kpartx -dv "$DISK_NAME"
 
 tar czvf $stemcell_dir/image *
 
 cd $stemcell_dir
-if [ "$bump_version" = true ]; then
-    sed -i.bak "s/version: .*/version: 0.0.${new_ver}/" stemcell.MF
-fi
-tar czvf $stemcell_tgz *
+# Change the order of how files are compressed to speed up version detection
+tar czvf ${OUT:-$stemcell_tgz} $(du -b * | sort -n | awk '{print $2}')
 
 echo "ALL DONE!!!"
