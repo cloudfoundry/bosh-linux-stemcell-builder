713b592f3d45c8384262e28c83197eb2fe6f3027
Maria Shaldybin <maria.shaldybin@broadcom.com>
2026-02-27 23:42:32 +0000

    Use bosh-enable-monit-access if available and fallback to nftables setup

--- stemcell_builder/stages/bosh_monit/assets/monit-access-helper.sh
+++ stemcell_builder/stages/bosh_monit/assets/monit-access-helper.sh
@@ -1,3 +1,13 @@
 permit_monit_access() {
-  /var/vcap/bosh/etc/bosh-enable-monit-access
+  local vcap_uid
+  vcap_uid="$(id -u vcap)"
+
+  if ! /var/vcap/bosh/etc/bosh-enable-monit-access "$vcap_uid" 2>/dev/null; then
+    if nft list chain inet bosh_agent monit_access_jobs &>/dev/null; then
+      if ! nft list chain inet bosh_agent monit_access_jobs 2>/dev/null | grep -q "skuid $vcap_uid"; then
+        nft add rule inet bosh_agent monit_access_jobs \
+          meta skuid "$vcap_uid" ip daddr 127.0.0.1 tcp dport 2822 accept
+      fi
+    fi
+  fi
 }
