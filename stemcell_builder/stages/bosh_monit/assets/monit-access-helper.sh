93677e1fc5cf8969a45cc3b78a21da6d582f6c15
Maria Shaldybin <maria.shaldybin@broadcom.com>
2026-02-27 22:00:41 +0000

    Run bosh-enable-monit-access as vcap if called by root
    Rules for root are already set by bosh agent. Set for vcap user for
    backwards compatiblity with old releases

--- stemcell_builder/stages/bosh_monit/assets/monit-access-helper.sh
+++ stemcell_builder/stages/bosh_monit/assets/monit-access-helper.sh
@@ -1,3 +1,7 @@
 permit_monit_access() {
-  /var/vcap/bosh/etc/bosh-enable-monit-access
+  if [ "$(id -u)" -eq 0 ]; then
+    sudo -u vcap /var/vcap/bosh/etc/bosh-enable-monit-access
+  else
+    /var/vcap/bosh/etc/bosh-enable-monit-access
+  fi
 }
