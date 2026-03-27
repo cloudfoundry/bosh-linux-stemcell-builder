fd7e70ab92664a99708a766880cafb42bb355af9
Julian Hjortshoj <julian.hjortshoj@broadcom.com>
2026-03-27 12:08:51 -0700

    install sfdisk in dockerfile
    instead of during stemcell building. Also clean up README instructions for docker image building so that they work for M series Mac users.

--- stemcell_builder/stages/image_create_efi/config.sh
+++ stemcell_builder/stages/image_create_efi/config.sh
@@ -5,8 +5,7 @@ set -e
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_config.bash
 
-# sfdisk will be installed at runtime if not available (see apply.sh)
-# kpartx is required for partition mapping
+assert_available sfdisk
 assert_available kpartx
 
 if [ -z "${image_create_disk_size:-}" ]
