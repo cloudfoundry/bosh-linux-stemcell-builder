f5d310b41888521382202fc198d4a77f066eb97a
Mike Yeromko <mykhailo.yeromko@sap.com>
2026-04-02 11:34:19 +0200

    Fix

--- bosh-stemcell/lib/bosh/stemcell/stage_collection.rb
+++ bosh-stemcell/lib/bosh/stemcell/stage_collection.rb
@@ -202,7 +202,6 @@ module Bosh::Stemcell
       [
         :system_parameters,
         :base_warden,
-        :base_systemd_clean,
         :bosh_clean,
         :bosh_harden,
         :bosh_clean_ssh,
