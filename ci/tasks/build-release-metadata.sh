da7e18be358589e069965455bcdecb492a25c30e
Ned Petrov <nedd.petrov@gmail.com>
2026-04-09 11:39:23 +0300

    Fix usn-log.json path in build-release-metadata.sh

--- ci/tasks/build-release-metadata.sh
+++ ci/tasks/build-release-metadata.sh
@@ -42,15 +42,15 @@ bosh_agent_version=$(cat "${REPO_PARENT}/bosh-linux-stemcell-builder/stemcell_bu
 
 if [[ "${OS_NAME}" == "ubuntu" ]]; then
   # Ensure URL for usn-log from metalink exists before attempting to download.
-  usn_log_json_file="${REPO_PARENT}/bosh-linux-stemcell-builder/${usn_log_json_file}/usn-log.json"
+  usn_log_json_file="${REPO_PARENT}/bosh-linux-stemcell-builder/usn-log.json"
   touch "${usn_log_json_file}"
   usn_metalink_path="${REPO_PARENT}/bosh-linux-stemcell-builder/bosh-stemcell/image-metalinks/${BRANCH}/${OS_NAME}-${OS_VERSION}.meta4"
-  if [[ -n "$(meta4 file-urls --metalink "${usn_metalink_path}" --file "${usn_log_json_file}")" ]]; then
+  if [[ -n "$(meta4 file-urls --metalink "${usn_metalink_path}" --file usn-log.json)" ]]; then
     meta4 file-download \
       --skip-hash-verification \
       --skip-signature-verification \
       --metalink "${usn_metalink_path}" \
-      --file "${usn_log_json_file}" \
+      --file usn-log.json \
       "${usn_log_json_file}"
   fi
 
