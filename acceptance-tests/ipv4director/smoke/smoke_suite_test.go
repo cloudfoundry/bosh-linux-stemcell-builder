de2094fb2b127d08b6e8832cf202809e97cbfe10
Matthew Kocher <matthew.kocher@broadcom.com>
2026-06-24 16:39:26 -0700

    ci: upload bpm-release in smoke tests
    it's now a dependency of syslog-release

--- acceptance-tests/ipv4director/smoke/smoke_suite_test.go
+++ acceptance-tests/ipv4director/smoke/smoke_suite_test.go
@@ -22,10 +22,12 @@ var _ = BeforeSuite(func() {
 	bosh = testhelpers.NewBOSH()
 	stemcellPath := testhelpers.RequireEnv("STEMCELL_PATH")
 	syslogReleasePath := testhelpers.RequireEnv("SYSLOG_RELEASE_PATH")
+	bpmReleasePath := testhelpers.RequireEnv("BPM_RELEASE_PATH")
 	osConfReleasePath := testhelpers.RequireEnv("OS_CONF_RELEASE_PATH")
 
 	bosh.UploadStemcell(stemcellPath)
 	bosh.UploadRelease(syslogReleasePath)
+	bosh.UploadRelease(bpmReleasePath)
 	bosh.UploadRelease(osConfReleasePath)
 	bosh.SafeDeploy()
 })
