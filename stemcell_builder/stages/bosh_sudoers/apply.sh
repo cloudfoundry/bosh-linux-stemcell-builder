df4787e88b0356bbde0cfdc0f2d9791acf671355
karthickaliappa <karthic.kaliappa@broadcom.com>
2026-08-07 12:44:55 +0530

    Revert changes to bosh_sudoers/apply.sh to keep file in original state

--- stemcell_builder/stages/bosh_sudoers/apply.sh
+++ stemcell_builder/stages/bosh_sudoers/apply.sh
@@ -6,9 +6,9 @@ base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 source $base_dir/lib/prelude_bosh.bash
 
-# enable sudoers.d drop-in support, and make sure we don't break anything
-cp -p "$chroot/etc/sudoers" "$chroot/etc/sudoers.save"
-echo '#includedir /etc/sudoers.d' >> "$chroot/etc/sudoers"
+# setup sudoers to use includedir, and make sure we don't break anything
+cp -p $chroot/etc/sudoers $chroot/etc/sudoers.save
+echo '#includedir /etc/sudoers.d' >> $chroot/etc/sudoers
 run_in_bosh_chroot $chroot "visudo -c"
 if [ $? -ne 0 ]; then
   echo "ERROR: bad sudoers file"
